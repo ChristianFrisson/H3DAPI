3dc796c2cffc57203266e82730cec9a2e564a6a7
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2006-02-24 06:50:03 +0000

    Added Fog, LocalFog and X3DFogObject nodes
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@195 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ src/Fog.cpp
@@ -0,0 +1,84 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file Fog.cpp
+/// \brief CPP file for Fog, X3D scene-graph node
+///
+//
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include "Fog.h"
+#include "X3DBindableNode.cpp"
+#include <GL/glew.h>
+
+using namespace H3D;
+
+// Add this node to the H3DNodeDatabase system.
+H3DNodeDatabase Fog::database( "Fog", 
+                               &(newInstance<Fog>), 
+                               typeid( Fog ),
+                               &X3DBindableNode< Fog >::database  );
+
+namespace FogInternals {
+  FIELDDB_ELEMENT( Fog, color, INPUT_OUTPUT );
+  FIELDDB_ELEMENT( Fog, fogType, INPUT_OUTPUT );
+  FIELDDB_ELEMENT( Fog, visibilityRange, INPUT_OUTPUT );
+}
+
+
+Fog::Fog( Inst< SFSetBind > _set_bind,
+          Inst< SFNode    > _metadata,
+          Inst< SFTime    > _bindTime,
+          Inst< SFBool    > _isBound,
+          Inst< SFColor   > _color,
+          Inst< SFString  > _fogType,
+          Inst< SFFloat   > _visibilityRange ) :
+  X3DBindableNode<Fog>( _set_bind, _metadata, _bindTime, _isBound ),
+  X3DFogObject( _color, _fogType, _visibilityRange ) {
+  
+  type_name = "Fog";
+  database.initFields( this );
+}
+
+void Fog::renderFog() {
+  const RGB &fog_color = color->getValue(); 
+  H3DFloat visibility_range = visibilityRange->getValue();
+  if( visibility_range != 0.0f ) {
+    GLfloat gl_fog_color[] = { fog_color.r, fog_color.g, fog_color.b, 0.0f };
+    const string &type = fogType->getValue();
+    if( type == "LINEAR" ) {
+      glFogi( GL_FOG_MODE, GL_LINEAR );
+    } else if( type == "EXPONENTIAL" ) {
+      glFogi( GL_FOG_MODE, GL_EXP );
+    } else {
+      cerr << "Invalid fogType \"" << type << "\". Must be one of \"LINEAR\""
+           << " or \"EXPONENTIAL\" (in \"" << getName() << "\" node )" << endl;
+    }
+    
+    glFogfv( GL_FOG_COLOR, gl_fog_color );
+    glFogf( GL_FOG_START, 0.0f );
+    glFogf( GL_FOG_END, scale_local_to_global * visibility_range );
+    glEnable( GL_FOG );
+  }
+}
---
+++ src/LocalFog.cpp
@@ -0,0 +1,88 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file LocalFog.cpp
+/// \brief CPP file for LocalFog, X3D scene-graph node
+///
+//
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include "LocalFog.h"
+#include <GL/glew.h>
+
+using namespace H3D;
+
+// Add this node to the H3DNodeDatabase system.
+H3DNodeDatabase LocalFog::database( "LocalFog", 
+                                    &(newInstance<LocalFog>), 
+                                    typeid( LocalFog ),
+                                    &X3DChildNode::database  );
+
+namespace LocalFogInternals {
+  FIELDDB_ELEMENT( LocalFog, color, INPUT_OUTPUT );
+  FIELDDB_ELEMENT( LocalFog, fogType, INPUT_OUTPUT );
+  FIELDDB_ELEMENT( LocalFog, visibilityRange, INPUT_OUTPUT );
+}
+
+LocalFog::LocalFog( Inst< SFNode    > _metadata,
+                    Inst< SFColor   > _color,
+                    Inst< SFString  > _fogType,
+                    Inst< SFFloat   > _visibilityRange ) :
+  X3DChildNode( _metadata ),
+  X3DFogObject( _color, _fogType, _visibilityRange ),
+  H3DRenderStateObject() {
+  
+  type_name = "LocalFog";
+  database.initFields( this );
+}
+
+void LocalFog::enableGraphicsState() {
+  glPushAttrib( GL_FOG_BIT );
+  H3DFloat visibility_range = visibilityRange->getValue();
+  if( visibility_range != 0.0f ) {
+    const RGB &localFog_color = color->getValue(); 
+    GLfloat gl_localFog_color[] = { localFog_color.r, 
+                                    localFog_color.g, 
+                                    localFog_color.b, 
+                                    0.0f };
+    const string &type = fogType->getValue();
+    if( type == "LINEAR" ) {
+      glFogi( GL_FOG_MODE, GL_LINEAR );
+    } else if( type == "EXPONENTIAL" ) {
+      glFogi( GL_FOG_MODE, GL_EXP );
+    } else {
+      cerr << "Invalid fogType \"" << type << "\". Must be one of \"LINEAR\""
+           << " or \"EXPONENTIAL\" (in \"" << getName() << "\" node )" << endl;
+    }
+    
+    glFogfv( GL_FOG_COLOR, gl_localFog_color );
+    glFogf( GL_FOG_START, 0.0f );
+    glFogf( GL_FOG_END, visibilityRange->getValue() );
+    glEnable( GL_FOG );
+  }
+}
+
+void LocalFog::disableGraphicsState() {
+  glPopAttrib();
+}
