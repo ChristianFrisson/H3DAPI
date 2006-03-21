037acde61c6842d628646aebe20bd09002dd2e31
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2006-03-21 04:48:11 +0000

    Using inifile for setting default values
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@226 610dfd11-2700-0410-9b68-9ae28eac5983

--- src/H3DLoad.cpp
+++ src/H3DLoad.cpp
@@ -16,7 +16,8 @@
 #include "MouseSensor.h"
 #include "DEFNodes.h"
 #include "Viewpoint.h"
-#include "Console.h"
+#include "DeviceInfo.h"
+#include "INIFile.h"
 
 #ifdef MACOSX
 #include "FreeImage.h"
@@ -74,26 +75,76 @@ class QuitAPIField: public AutoUpdate< SFString > {
 
 int main(int argc, char* argv[]) {
   Console.setShowLevel(false);
-  Console(4) << "H3DLoad build 204" << endl;
+  
   if (argc < 2){
     Console(4) << "Usage: " << argv[0] << " <X3D file>" << endl;
     return 1;
   }
 
   const char* xml_file = argv[1];
-  //const char* xml_file = "Test.x3d";
   try {
-    // TODO: move someplace where is is initialized once
-    //		  XMLPlatformUtils::Initialize();
-
     AutoRef< KeySensor > ks( new KeySensor );
     AutoRef< MouseSensor > ms( new MouseSensor );
     X3D::DEFNodes dn;
     KeyRotation *kr = new KeyRotation;
     QuitAPIField *quit_api = new QuitAPIField;
     AutoRef< Transform > t( new Transform );
-
     Console(3) << "Loading " << xml_file << endl;
+    AutoRef< Node > device_info;
+    AutoRef< Node > viewpoint;
+    AutoRef< Scene > scene( new Scene );
+
+    char * buffer;
+
+    buffer = getenv ("H3D_DEFAULT_DEVICEINFO");
+
+    if( buffer ) {
+      try {
+        device_info = X3D::createX3DNodeFromURL( buffer );
+      } catch( const Exception::H3DException &e ) {
+        Console(3) << "Warning: Could not create default DeviceInfo node "
+                   << "from file \"" << buffer << "\" pointed to by "
+                   << "environment variable H3D_DEFAULT_DEVICEINFO. " 
+                   << e << endl;
+      }
+    } else {
+      // ini file setting
+      buffer = getenv( "H3D_ROOT" );
+
+      if( buffer ) {
+        string path = string( buffer ) + 
+          "/settings/current/deviceinfo.x3d";
+        try {
+          device_info = X3D::createX3DNodeFromURL( path );
+        } catch( const Exception::H3DException &e ) {
+          Console(3) << "Warning: Could not create default DeviceInfo node "
+                     << "from file \"" << path << "\" "
+                     << e << endl;
+        }
+      }
+    }
+    
+    DeviceInfo *di = DeviceInfo::getActive();
+    if( di ) {
+      string stylus_path = string( buffer ) + 
+        "/settings/current/stylus.x3d";
+      AutoRef< Node > default_stylus;
+      try {
+        default_stylus = X3D::createX3DNodeFromURL( stylus_path );
+      } catch( const Exception::H3DException &e ) {
+        Console( 4 ) << "Warning: Could not create default stylus"
+                     << "from file \"" << stylus_path << "\" "
+                     << e << endl;
+      }
+      
+      for( DeviceInfo::MFDevice::const_iterator i = di->device->begin();
+           i != di->device->end(); i++ ) {
+        H3DHapticsDevice *d = static_cast< H3DHapticsDevice * >(*i);
+        if( !d->stylus->getValue() )
+          d->stylus->setValue( default_stylus );
+      }
+    }
+
     t->children->push_back( X3D::createX3DFromURL( xml_file, 
                                                    &dn ) );
     
@@ -108,28 +159,69 @@ int main(int argc, char* argv[]) {
     
     // create a Viewpoint if it does not exist.
     if( !Viewpoint::getActive() ) {
-      Console(2) << "No Viewpoint node found, creating one." << endl;
-
-      Viewpoint *vp = new Viewpoint;
-      vp->position->setValue( Vec3f( 0, 0, 0.6f ) );
+      buffer = getenv ("H3D_DEFAULT_VIEWPOINT");
+      if( buffer ) {
+        try {
+          viewpoint = X3D::createX3DNodeFromURL( buffer );
+        } catch( const Exception::H3DException &e ) {
+          Console(3) << "Warning: Could not create default Viewpoint node "
+                     << "from file \"" << buffer << "\" pointed to by "
+                     << "environment variable H3D_DEFAULT_VIEWPOINT. " 
+                     << e << endl;
+        }
+      } else {
+        // ini file setting
+        buffer = getenv( "H3D_ROOT" );
+        
+        if( buffer ) {
+          string path = string( buffer ) + 
+            "/settings/current/viewpoint.x3d";
+          try {
+            viewpoint = X3D::createX3DNodeFromURL( path );
+          } catch( const Exception::H3DException &e ) {
+            Console(3) << "Warning: Could not create default Viewpoint node "
+                       << "from file \"" << path << "\" "
+                       << e << endl;
+          }
+        } 
+      }
     }
-                         
+
+    if( X3DBindableNode::getStack( "DeviceInfo" ).size() > 1 ) {
+      device_info.reset( NULL );
+    }
+
     g->children->push_back( t.get() );
 
     bool fullscreen = false;
     bool mirrored = false;
     string render_mode = "MONO";
 
+    buffer = getenv( "H3D_ROOT" );
+    if( buffer ) {
+      INIFile ini_file( string( buffer ) + "/settings/h3dload.ini" );
+      if( ini_file.hasOption( "graphical", "mirrored" ) ) {
+        mirrored = ini_file.getBoolean( "graphical", "mirrored" );
+      }
+      if( ini_file.hasOption( "graphical", "fullscreen" ) ) {
+        fullscreen = ini_file.getBoolean( "graphical", "fullscreen" );
+      }
+      if( ini_file.hasOption( "graphical", "rendermode" ) ) {
+        render_mode = ini_file.get( "graphical", "rendermode" );
+      }
+    }
+
     // check enviroment variables to see if the window parameters has
     // been set by the user.
-    char * buffer;
     buffer = getenv ("H3D_FULLSCREEN");
     if( buffer!=NULL ) {
       if (strcmp( buffer, "TRUE" ) == 0 )
         fullscreen = true;
       else if(strcmp( buffer, "FALSE" ) != 0 )
-        Console(4) << "Invalid valid value \"" << buffer << "\" on environment "
-                   << "variable H3D_FULLSCREEN. Must be TRUE or FALSE. "<< endl;
+        Console(4) << "Invalid valid value \"" << buffer 
+                   << "\" on environment "
+                   << "variable H3D_FULLSCREEN. Must be TRUE or FALSE. "
+                   << endl;
     }
 
     buffer = getenv ("H3D_MIRRORED");
@@ -137,8 +229,9 @@ int main(int argc, char* argv[]) {
       if (strcmp( buffer, "TRUE" ) == 0 )
         mirrored = true;
       else if(strcmp( buffer, "FALSE" ) != 0 )
-        Console(4) << "Invalid valid value \"" << buffer << "\" on environment "
-                   << "variable H3D_MIRRORED. Must be TRUE or FALSE. "<< endl;
+        Console(4) << "Invalid valid value \"" << buffer 
+                   << "\" on environment "
+             << "variable H3D_MIRRORED. Must be TRUE or FALSE. "<< endl;
     }
 
     buffer = getenv ("H3D_RENDERMODE");
@@ -146,14 +239,15 @@ int main(int argc, char* argv[]) {
       render_mode = buffer;
     }
 		  
-    AutoRef< Scene > scene( new Scene );
     // create a window to display
     GLUTWindow *glwindow = new GLUTWindow;
+    GLUTWindow *glwindow2 = new GLUTWindow;
 
     glwindow->fullscreen->setValue( fullscreen );
     glwindow->mirrored->setValue( mirrored );
     glwindow->renderMode->setValue( render_mode );
     scene->window->push_back( glwindow );
+ //   scene->window->push_back( glwindow2 );
     scene->sceneRoot->setValue( g.get() );
     Scene::mainLoop();
   }
@@ -161,8 +255,7 @@ int main(int argc, char* argv[]) {
   }
 
   catch (const Exception::H3DException &e) {
-    Console(4) << "Exception:" << endl;
-    Console(4) << "   " << e << endl;
+    Console(4) << e << endl;
   }
 
 }
---
+++ src/INIFile.cpp
@@ -0,0 +1,146 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+/// \file INIFile.cpp Class for parsing an .ini file
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include "INIFile.h"
+#include <fstream>
+
+using namespace std;
+using namespace H3D;
+
+
+namespace { 
+
+  bool nonCaseEquals( const string &s1, 
+                      const string &s2 ) {
+    if( s1.size() != s2.size() ) return false;
+    
+    for( unsigned int i = 0; i < s1.size(); i++ ) {
+      if( toupper( s1[i]) != toupper( s2[i] ) )
+        return false;
+    }
+    return true;
+  }
+
+  void cleanup( std::string& line ) {
+    std::string copy = line;
+    unsigned begin = 0;
+    while ( begin != line.size() && isspace( line[ begin ] ) ) ++begin;
+    
+    bool inQuote = false;
+    unsigned end;
+    for ( end = begin; end != line.size(); ++end ) {
+      if ( line[ end ] == '\"' ) {
+        inQuote = !inQuote;
+      } else if ( line[ end ] == '#' && !inQuote ) {
+        break;
+      } else if ( line[ end ] == '\\' ) {
+        ++end; // ignore next character
+        if ( end == line.size() ) {
+          std::cerr << "Error parsing file: \\ character "
+            "at the end of line (sorry, not supported)\n";
+          break;
+        }
+      }
+    }
+    while ( end > begin && isspace( line[ end - 1 ] ) ) --end;
+    line.assign( line, begin, end - begin );
+    //std::cerr << "cleanup(\'" << copy << "\') = \'" << line << "\'\n";
+  }
+
+  bool parseGroupName( string line, string& group ) {
+    if ( line[ 0 ] != '[' ) return false;
+    if ( line[ line.size() - 1 ] != ']' ) return false;
+    
+    group.assign( line, 1, line.size() - 2 );
+    return true;
+  }
+}
+
+void INIFile::parseLine( string line, string group, unsigned count ) {
+  unsigned equals = line.find( '=' );
+  if ( equals == string::npos ) {
+    std::cerr << "Could not parse line " << count << ", ignoring.\n";
+    return;
+  }
+
+  string name( line, 0, equals );
+  cleanup( name );
+  
+  string value( string( line, equals + 1, string::npos ) );
+  cleanup( value );
+  parsed_[ group ][ name ] = value;
+}
+
+INIFile::INIFile( const std::string& file ) {
+  ifstream is( file.c_str() );
+  parseStream( is );
+  is.close();
+}
+
+INIFile::INIFile( std::istream& in ) {
+  parseStream( in );
+}
+
+void INIFile::parseStream( std::istream &in ) {
+  unsigned count = 0;
+  string line;
+  bool inside = false;
+
+  string group;
+  while ( getline( in, line ) ) {
+    ++count;
+    cleanup( line );
+    if ( line.empty() ) continue;
+    if ( parseGroupName( line, group ) ) {
+      inside = true;
+    } else if ( inside ) {
+      parseLine( line, group, count );
+    }
+  }
+}
+
+string INIFile::get( string group, string name ) const {
+  map<string, map<string, string> >::const_iterator one = 
+    parsed_.find( group );
+  if ( one == parsed_.end() ) return "";
+  
+  map<string, string>::const_iterator two = 
+    one->second.find( name );
+  if ( two == one->second.end() ) return "";
+  return two->second;
+}
+
+bool INIFile::getBoolean( string group, string name ) const {
+  string v = get( group, name ); 
+  return( nonCaseEquals( v, "1" ) ||
+          nonCaseEquals( v, "yes" ) ||
+          nonCaseEquals( v, "true" ) ||
+          nonCaseEquals( v, "on" ) );
+}
+
+bool INIFile::hasOption( string group, string name ) const {
+  return get( group, name ) != "";
+}
