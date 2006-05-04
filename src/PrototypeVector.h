c4c3f3f73156d2231037779b300a1a2ea4b351cf
mark <mark@610dfd11-2700-0410-9b68-9ae28eac5983>
2006-05-04 06:46:43 +0000

    Moved PrototypeVector out to a separate .h file
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@298 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ src/PrototypeVector.h
@@ -0,0 +1,43 @@
+//////////////////////////////////////////////////////////////////////////////
+//    H3D API.   Copyright 2004, Danel Evestedt, Mark Dixon
+//    All Rights Reserved
+//
+/// \file PrototypeVector.h  Used by VRMLParser and X3D to implement
+/// a PROTO declaration.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef __PROTOTYPEVECTOR_H__
+#define __PROTOTYPEVECTOR_H__
+#include "ProtoDeclaration.h"
+
+using namespace std;
+
+namespace H3D {
+  namespace X3D {
+
+    class PrototypeVector: public AutoPtrVector< ProtoDeclaration > {
+    public:
+      PrototypeVector(): first_proto( NULL ) {}
+      ProtoDeclaration *getProtoDeclaration( const string & name ) {
+        for( PrototypeVector::iterator i = begin(); i != end(); i++ ) {
+          if( (*i)->getName() == name )
+            return *i;
+        }
+        return NULL;
+      }
+      inline ProtoDeclaration *getFirstProtoDeclaration() {
+        return first_proto;
+      }
+      
+      inline void setFirstProtoDeclaration( ProtoDeclaration *pd ) {
+        first_proto = pd;
+      }
+    protected:
+      ProtoDeclaration *first_proto;
+    };
+  }
+};
+
+#endif
+    
