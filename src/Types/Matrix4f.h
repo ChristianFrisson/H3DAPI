8531066b5d1ccf2e279835d4e4874b9d63683898
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2006-05-11 06:00:28 +0000

    Added transpose functions
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@320 610dfd11-2700-0410-9b68-9ae28eac5983

--- src/Types/Matrix4f.h
+++ src/Types/Matrix4f.h
@@ -100,6 +100,14 @@ namespace H3D {
       ///
       Matrix4f transformInverse() const;
 	
+      /// Returns the transpose of the matrix.
+      inline Matrix4f transpose() const {
+        return Matrix4f( m[0][0], m[1][0], m[2][0], m[3][0],
+												 m[0][1], m[1][1], m[2][1], m[3][1],
+												 m[0][2], m[1][2], m[2][2], m[3][2],
+                         m[0][3], m[1][3], m[2][3], m[3][3] );
+      };
+
       /// Returns the inverse of the matrix.
       Matrix4f inverse() const;
 
