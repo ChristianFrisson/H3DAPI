476141cd358371f784eeab75da082e97e7a03294
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2006-05-11 04:08:10 +0000

    Added command line arguments to H3DLoad and some more options. Thanks KJ!
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@318 610dfd11-2700-0410-9b68-9ae28eac5983

--- src/H3DLoad.cpp
+++ src/H3DLoad.cpp
@@ -74,15 +74,227 @@ class QuitAPIField: public AutoUpdate< SFString > {
 };
 
 
+#define GET4(ENV,GROUP,VAR,DEFAULT) \
+( getenv(ENV) ?                     \
+  string(getenv(ENV)) :             \
+  ( ini_file.hasOption(GROUP,VAR) ? \
+    ini_file.get( GROUP, VAR ) :    \
+    DEFAULT ) )
+
+#define GET_ENV_INI_DEFAULT(ENV,PATH,GROUP,VAR,DEFAULT) \
+( getenv(ENV) ?                                         \
+  string(getenv(ENV)) :                                 \
+  ( ini_file.hasOption(GROUP,VAR) ?                     \
+    PATH + ini_file.get( GROUP, VAR ) :                 \
+    DEFAULT ) )
+
+#define GET_INT(GROUP,VAR,DEFAULT)  \
+( ini_file.hasOption(GROUP,VAR) ? \
+  atoi(ini_file.get( GROUP, VAR ).c_str()) :    \
+  DEFAULT )
+
+#define GET_BOOL(GROUP,VAR,DEFAULT)   \
+( ini_file.hasOption(GROUP,VAR) ?     \
+  ini_file.getBoolean( GROUP, VAR ) : \
+  DEFAULT )
+
+
 int main(int argc, char* argv[]) {
-  Console.setShowLevel(false);
   
-  if (argc < 2){
-    Console(4) << "Usage: " << argv[0] << " <X3D file>" << endl;
+  
+  // Settings and command line arguments ---
+  
+  // Help message
+  
+  string command = argv[0];
+  string help_message = "\n";
+  help_message += "Usage: " + command + " [Options] <X3D file>\n";
+  help_message += "\n";
+  help_message += "Options:\n";
+  help_message += "\n";
+  help_message += " -m --mirror            Flip screen upside down\n";
+  help_message += " -M --no-mirror         Surpress flipping screen\n";
+  help_message += " -f --fullscreen        Open in fullscreen mode\n";
+  help_message += " -F --no-fullscreen     Surpress fullscreen mode\n";
+  help_message += "    --screen=<W>x<H>    Set width and height for window\n";
+  help_message += "\n";
+  help_message += "    --deviceinfo=<file> Use <file> as device info\n";
+  help_message += "    --stylus=<file>     Use <file> as stylus model\n";
+  help_message += "    --viewpoint=<file>  Use <file> as viewpoint\n";
+  help_message += "    --rendermode=<mode> Use render mode <mode>\n";
+  help_message += "\n";
+  help_message += " -h --help              This help message\n";
+  help_message += "\n";
+  
+  // H3D root, XML file and setup file
+  
+  vector<string> xml_files;
+  string h3d_root = getenv( "H3D_ROOT" );
+  INIFile ini_file( h3d_root + "/settings/h3dload.ini" );
+  
+  // Console settings
+  
+  bool console_show_time = GET_BOOL("console", "show_time", false);
+  bool console_show_level = GET_BOOL("console", "show_level", false);
+  int console_output_level = GET_INT("console", "output_level", 3 );
+
+  ostream *console_ostream = 0;
+  string ostream_str = GET4("H3D_CONSOLE_OSTREAM",
+                            "console","ostream","cerr");
+  if ( ostream_str == "cerr" )
+    console_ostream = &cerr;
+  else if( ostream_str == "cout" )
+    console_ostream = &cout;
+  else
+    Console(4) << "Invalid valid value \"" <<  ostream_str
+               << "\" on ostream variable. Must be cerr or cout."<< endl;
+  
+  Console.setShowTime(console_show_time);
+  Console.setShowLevel(console_show_level);
+  Console.setOutputLevel(console_output_level);
+  Console.setOutputStream(*console_ostream);
+  
+  // Graphics, devices, models and such
+  
+  string deviceinfo_file =
+    GET_ENV_INI_DEFAULT( "H3D_DEFAULT_DEVICEINFO",
+                         h3d_root + "/settings/common/deviceinfo/",
+                         "haptics device","deviceinfo",
+                         h3d_root + "/settings/current/deviceinfo.x3d" );
+  
+  string stylus_file =
+    GET_ENV_INI_DEFAULT( "H3D_STYLUS",
+                         h3d_root + "/settings/common/stylus/",
+                         "haptics device","stylus",
+                         h3d_root + "/settings/current/stylus.x3d" );
+  
+  string viewpoint_file =
+    GET_ENV_INI_DEFAULT( "H3D_DEFAULT_VIEWPOINT",
+                         h3d_root + "/settings/common/viewpoint/",
+                         "graphical", "viewpoint",
+                         h3d_root + "/settings/current/viewpoint.x3d" );
+
+  string render_mode = GET4( "H3D_RENDERMODE",
+                             "graphical", "rendermode",
+                             "MONO" );
+  
+  int width = GET_INT("graphical", "width", 640 );
+  int height = GET_INT("graphical", "height", 480 );
+  
+  bool fullscreen    = GET_BOOL("graphical", "fullscreen", false);
+  if( char *buffer = getenv("H3D_FULLSCREEN") ) {
+    if (strcmp( buffer, "TRUE" ) == 0 )
+      fullscreen = true;
+    else if(strcmp( buffer, "FALSE" ) != 0 )
+      Console(4) << "Invalid valid value \"" << buffer 
+                 << "\" on environment "
+                 << "variable H3D_FULLSCREEN. Must be TRUE or FALSE. "
+                 << endl;
+  }
+  
+  bool mirrored      = GET_BOOL("graphical", "mirrored", false);
+  if( char *buffer = getenv("H3D_MIRRORED") ) {
+    if (strcmp( buffer, "TRUE" ) == 0 )
+      mirrored = true;
+    else if(strcmp( buffer, "FALSE" ) != 0 )
+      Console(4) << "Invalid valid value \"" << buffer 
+                 << "\" on environment "
+                 << "variable H3D_MIRRORED. Must be TRUE or FALSE. "<< endl;
+  }
+  
+
+  // Command line arguments ---
+  
+  for( int i = 1 ; i < argc ; i++ ){
+    
+    if( argv[i][0] != '-' ){
+      xml_files.push_back( string(argv[i]) );
+      continue;
+    }
+    
+    switch(argv[i][1]){
+      
+      // Long arguments ---
+      
+    case '-':
+      
+      if( !strcmp(argv[i]+2,"help") ){
+        std::cout << help_message << endl;
+        return 0;
+      }
+      
+      else if( !strcmp(argv[i]+2,"mirror") ){
+        mirrored = true; }
+      
+      else if( !strcmp(argv[i]+2,"no-mirror") ){
+        mirrored = false; }
+      
+      else if( !strcmp(argv[i]+2,"fullscreen") ){
+        fullscreen = true; }
+      
+      else if( !strcmp(argv[i]+2,"no-fullscreen") ){
+        fullscreen = false; }
+      
+      else if( !strncmp(argv[i]+2,"screen=",
+                        strlen("screen=")) ){
+        height = atoi( strstr(argv[i],"x")+1 );
+        *strstr(argv[i],"x") = '\0';
+        width = atoi( strstr(argv[i],"=")+1 );
+      }
+      
+      else if( !strncmp(argv[i]+2,"deviceinfo=",
+                        strlen("deviceinfo=")) ){
+        deviceinfo_file = strstr(argv[i],"=")+1; }
+      
+      else if( !strncmp(argv[i]+2,"stylus=",
+                        strlen("stylus=")) ){
+        stylus_file = strstr(argv[i],"=")+1; }
+      
+      else if( !strncmp(argv[i]+2,"viewpoint=",
+                        strlen("viewpoint=")) ){
+        viewpoint_file = strstr(argv[i],"=")+1; }
+      
+      else if( !strncmp(argv[i]+2,"rendermode=",
+                        strlen("rendermode=")) ){
+        render_mode = strstr(argv[i],"=")+1; }
+      
+      else {
+        Console(4) << "Unknown argument "
+                   << "'" << argv[i] << "'" << endl; }
+      break;
+      
+      // Short arguments ---
+      
+    case 'm':
+      mirrored = true;
+      break;
+      
+    case 'M':
+      mirrored = false;
+      break;
+      
+    case 'f':
+      fullscreen = true;
+      break;
+      
+    case 'F':
+      fullscreen = false;
+      break;
+      
+    default:
+      Console(4) << "Unknown argument "
+                 << "'" << argv[i] << "'" << endl;
+    }
+  }
+  
+  if (!xml_files.size()){
+    Console(4) << help_message << endl;
     return 1;
   }
-
-  string xml_file = argv[1];
+  
+  
+  // Loading X3D file and setting up VR environment ---
+  
   try {
     AutoRef< KeySensor > ks( new KeySensor );
     AutoRef< MouseSensor > ms( new MouseSensor );
@@ -90,51 +302,28 @@ int main(int argc, char* argv[]) {
     KeyRotation *kr = new KeyRotation;
     QuitAPIField *quit_api = new QuitAPIField;
     AutoRef< Transform > t( new Transform );
-    Console(3) << "Loading " << xml_file << endl;
     AutoRef< Node > device_info;
     AutoRef< Node > viewpoint;
     AutoRef< Scene > scene( new Scene );
-
-    char * buffer;
-
-    buffer = getenv ("H3D_DEFAULT_DEVICEINFO");
-
-    if( buffer ) {
+    
+    if( deviceinfo_file.size() ){
       try {
-        device_info = X3D::createX3DNodeFromURL( buffer );
+        device_info = X3D::createX3DNodeFromURL( deviceinfo_file );
       } catch( const Exception::H3DException &e ) {
         Console(3) << "Warning: Could not create default DeviceInfo node "
-                   << "from file \"" << buffer << "\" pointed to by "
-                   << "environment variable H3D_DEFAULT_DEVICEINFO. " 
+                   << "from file \"" << deviceinfo_file << "\": "
                    << e << endl;
-      }
-    } else {
-      // ini file setting
-      buffer = getenv( "H3D_ROOT" );
-
-      if( buffer ) {
-        string path = string( buffer ) + 
-          "/settings/current/deviceinfo.x3d";
-        try {
-          device_info = X3D::createX3DNodeFromURL( path );
-        } catch( const Exception::H3DException &e ) {
-          Console(3) << "Warning: Could not create default DeviceInfo node "
-                     << "from file \"" << path << "\" "
-                     << e << endl;
-        }
       }
     }
     
     DeviceInfo *di = DeviceInfo::getActive();
-    if( di ) {
-      string stylus_path = string( buffer ) + 
-        "/settings/current/stylus.x3d";
+    if( di && stylus_file.size() ) {
       AutoRef< Node > default_stylus;
       try {
-        default_stylus = X3D::createX3DNodeFromURL( stylus_path );
+        default_stylus = X3D::createX3DNodeFromURL( stylus_file );
       } catch( const Exception::H3DException &e ) {
-        Console( 4 ) << "Warning: Could not create default stylus"
-                     << "from file \"" << stylus_path << "\" "
+        Console( 4 ) << "Warning: Could not create default stylus "
+                     << "from file \"" << stylus_file << "\": "
                      << e << endl;
       }
       
@@ -145,16 +334,20 @@ int main(int argc, char* argv[]) {
           d->stylus->setValue( default_stylus );
       }
     }
-
-    if ( xml_file.size() > 4 && 
-         xml_file.find( ".wrl", xml_file.size()-5 ) != string::npos )
-      t->children->push_back( X3D::createVRMLFromURL( xml_file, 
-                                                     &dn ) );
-    else
-      t->children->push_back( X3D::createX3DFromURL( xml_file, 
-                                                      &dn ) );
-
-
+    
+    for( vector<string>::iterator file = xml_files.begin() ;
+         file != xml_files.end() ; file++ ){
+      Console(3) << "Loading " << *file << endl;
+      if ( file->size() > 4 && 
+           file->find( ".wrl", file->size()-5 ) != string::npos )
+        t->children->push_back( X3D::createVRMLFromURL( *file, 
+                                                        &dn ) );
+      else
+        t->children->push_back( X3D::createX3DFromURL( *file, 
+                                                       &dn ) );
+    }
+    
+    
     
     // set up routes to rotate the model
     ks->keyPress->route( quit_api );
@@ -166,96 +359,32 @@ int main(int argc, char* argv[]) {
     AutoRef< Group > g( new Group );     
     
     // create a Viewpoint if it does not exist.
-    if( !Viewpoint::getActive() ) {
-      buffer = getenv ("H3D_DEFAULT_VIEWPOINT");
-      if( buffer ) {
-        try {
-          viewpoint = X3D::createX3DNodeFromURL( buffer );
-        } catch( const Exception::H3DException &e ) {
-          Console(3) << "Warning: Could not create default Viewpoint node "
-                     << "from file \"" << buffer << "\" pointed to by "
-                     << "environment variable H3D_DEFAULT_VIEWPOINT. " 
-                     << e << endl;
-        }
-      } else {
-        // ini file setting
-        buffer = getenv( "H3D_ROOT" );
-        
-        if( buffer ) {
-          string path = string( buffer ) + 
-            "/settings/current/viewpoint.x3d";
-          try {
-            viewpoint = X3D::createX3DNodeFromURL( path );
-          } catch( const Exception::H3DException &e ) {
-            Console(3) << "Warning: Could not create default Viewpoint node "
-                       << "from file \"" << path << "\" "
-                       << e << endl;
-          }
-        } 
+    if( !Viewpoint::getActive() && viewpoint_file.size() ) {
+      try {
+        viewpoint = X3D::createX3DNodeFromURL( viewpoint_file );
+      } catch( const Exception::H3DException &e ) {
+        Console(3) << "Warning: Could not create default Viewpoint node "
+                   << "from file \"" << viewpoint_file << "\": "
+                   << e << endl;
       }
     }
-
+    
     if( X3DBindableNode::getStack( "DeviceInfo" ).size() > 1 ) {
       device_info.reset( NULL );
     }
 
     g->children->push_back( t.get() );
 
-    bool fullscreen = false;
-    bool mirrored = false;
-    string render_mode = "MONO";
-
-    buffer = getenv( "H3D_ROOT" );
-    if( buffer ) {
-      INIFile ini_file( string( buffer ) + "/settings/h3dload.ini" );
-      if( ini_file.hasOption( "graphical", "mirrored" ) ) {
-        mirrored = ini_file.getBoolean( "graphical", "mirrored" );
-      }
-      if( ini_file.hasOption( "graphical", "fullscreen" ) ) {
-        fullscreen = ini_file.getBoolean( "graphical", "fullscreen" );
-      }
-      if( ini_file.hasOption( "graphical", "rendermode" ) ) {
-        render_mode = ini_file.get( "graphical", "rendermode" );
-      }
-    }
-
-    // check enviroment variables to see if the window parameters has
-    // been set by the user.
-    buffer = getenv ("H3D_FULLSCREEN");
-    if( buffer!=NULL ) {
-      if (strcmp( buffer, "TRUE" ) == 0 )
-        fullscreen = true;
-      else if(strcmp( buffer, "FALSE" ) != 0 )
-        Console(4) << "Invalid valid value \"" << buffer 
-                   << "\" on environment "
-                   << "variable H3D_FULLSCREEN. Must be TRUE or FALSE. "
-                   << endl;
-    }
-
-    buffer = getenv ("H3D_MIRRORED");
-    if( buffer!=NULL ) {
-      if (strcmp( buffer, "TRUE" ) == 0 )
-        mirrored = true;
-      else if(strcmp( buffer, "FALSE" ) != 0 )
-        Console(4) << "Invalid valid value \"" << buffer 
-                   << "\" on environment "
-             << "variable H3D_MIRRORED. Must be TRUE or FALSE. "<< endl;
-    }
-
-    buffer = getenv ("H3D_RENDERMODE");
-    if( buffer!=NULL ) {
-      render_mode = buffer;
-    }
-		  
+    
     // create a window to display
     GLUTWindow *glwindow = new GLUTWindow;
-    GLUTWindow *glwindow2 = new GLUTWindow;
 
     glwindow->fullscreen->setValue( fullscreen );
     glwindow->mirrored->setValue( mirrored );
     glwindow->renderMode->setValue( render_mode );
+    glwindow->width->setValue(width);
+    glwindow->height->setValue(height);
     scene->window->push_back( glwindow );
- //   scene->window->push_back( glwindow2 );
     scene->sceneRoot->setValue( g.get() );
     Scene::mainLoop();
   }
