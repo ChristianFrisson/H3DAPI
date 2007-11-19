45fcc2d7774bc824365d5fc0eaaca9fb118f8c9b
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2007-11-19 19:00:45 +0000

    Added HapticMasterDevice
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@893 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ src/HapticMasterDevice.cpp
@@ -0,0 +1,88 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file HapticMasterDevice.cpp
+/// \brief cpp file for HapticMasterDevice
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+#include <H3D/HapticMasterDevice.h>
+#include <HAPI/HapticMasterDevice.h>
+
+using namespace H3D;
+
+H3DNodeDatabase HapticMasterDevice::database( "HapticMasterDevice", 
+                                          &(newInstance<HapticMasterDevice>),
+                                          typeid( HapticMasterDevice ),
+                                          &H3DHapticsDevice::database ); 
+namespace HapticMasterDeviceInternals {
+}
+
+/// Constructor.
+HapticMasterDevice::HapticMasterDevice( 
+               Inst< SFVec3f         > _devicePosition,
+               Inst< SFRotation      > _deviceOrientation,
+               Inst< TrackerPosition > _trackerPosition,
+               Inst< TrackerOrientation > _trackerOrientation,
+               Inst< PosCalibration  > _positionCalibration,
+               Inst< OrnCalibration  > _orientationCalibration,
+               Inst< SFVec3f         > _proxyPosition,
+               Inst< WeightedProxy   > _weightedProxyPosition,     
+               Inst< SFFloat         > _proxyWeighting,
+               Inst< MainButton      > _mainButton,
+               Inst< SecondaryButton > _secondary_button,
+               Inst< SFInt32         > _buttons,
+               Inst< SFVec3f         > _force,
+               Inst< SFVec3f         > _torque,
+               Inst< SFInt32         > _inputDOF,
+               Inst< SFInt32         > _outputDOF,
+               Inst< SFInt32         > _hapticsRate,
+               Inst< SFNode          > _stylus,
+               Inst< SFFloat         > _proxyRadius ) :
+  H3DHapticsDevice( _devicePosition, _deviceOrientation, _trackerPosition,
+              _trackerOrientation, _positionCalibration, 
+              _orientationCalibration, _proxyPosition,
+              _weightedProxyPosition, _proxyWeighting, _mainButton,
+                    _secondary_button, _buttons,
+              _force, _torque, _inputDOF, _outputDOF, _hapticsRate,
+              _stylus ) { 
+
+  type_name = "HapticMasterDevice";  
+  database.initFields( this );
+  hapi_device.reset(0);
+}
+
+void HapticMasterDevice::initialize() {
+  H3DHapticsDevice::initialize();
+#ifdef HAVE_HAPTIC_MASTER_API
+  hapi_device.reset( new HAPI::HapticMasterDevice() );
+#else
+  Console(4) << "Cannot use HapticMasterDevice. HAPI compiled without"
+	     << " HapticMaster support. Recompile HAPI with "
+	     << "HAVE_HAPTIC_MASTER_API defined"
+	     << " in order to use it." << endl;
+#endif
+}
+
