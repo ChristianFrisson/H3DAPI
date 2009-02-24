18a0cccd94594b5a830d578e07d428b7346ecb09
juliana <juliana@610dfd11-2700-0410-9b68-9ae28eac5983>
2009-02-24 07:38:24 +0000

    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@1153 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ src/CoordinateDouble.cpp
@@ -0,0 +1,71 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file CoordinateDouble.cpp
+/// \brief CPP file for CoordinateDouble, X3D scene-graph node
+///
+//
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <H3D/CoordinateDouble.h>
+
+using namespace H3D;
+
+// Add this node to the H3DNodeDatabase system
+H3DNodeDatabase CoordinateDouble::database( "CoordinateDouble",
+                                            &(newInstance<CoordinateDouble>),
+                                            typeid( CoordinateDouble ),
+                                            &X3DCoordinateNode::database );
+
+namespace CoordinateDoubleInternals {
+  FIELDDB_ELEMENT( CoordinateDouble, point, INPUT_OUTPUT );
+}
+
+CoordinateDouble::CoordinateDouble( 
+                            Inst< SFNode  > _metadata,
+                            Inst< MFVec3d > _point ) :
+  X3DCoordinateNode( _metadata ),
+  point( _point ) {
+
+  type_name = "CoordinateDouble";
+  database.initFields( this );
+
+  point->route( propertyChanged );
+}
+
+/// Perform the OpenGL commands to render all verties as a vertex
+/// array.
+void CoordinateDouble::renderArray() {
+  if ( !point->empty() ) {
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer( 3, GL_DOUBLE, 0,
+                     &(*point->begin()) );
+
+  }
+}
+
+/// Disable the array state enabled in renderArray().
+void CoordinateDouble::disableArray() {
+  glDisableClientState(GL_VERTEX_ARRAY);
+}
