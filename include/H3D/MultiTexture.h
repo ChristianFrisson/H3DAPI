b8e30519377cf8458c2bcb51145e3cc37f478190
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2009-03-13 14:31:16 +0000

    Updated comments for MultiTexture.
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@1165 610dfd11-2700-0410-9b68-9ae28eac5983

--- include/H3D/MultiTexture.h
+++ include/H3D/MultiTexture.h
@@ -39,8 +39,6 @@
 namespace H3D {
   /// \ingroup X3DNodes 
   /// \class MultiTexture
-  /// Partially implemented.
-  ///
   /// MultiTexture enables the application of several individual textures
   /// to a 3D object to achieve a more complex visual effect. 
   /// MultiTexture can be used as a value for the texture field in an 
@@ -51,13 +49,57 @@ namespace H3D {
   ///
   /// - The color and alpha fields define base RGB and alpha values for SELECT
   ///   mode operations.
-  /// - The mode field controls the type of blending operation. The only
-  ///   available mode at this time is "MODULATE".
+  /// - The mode field controls the type of blending operation. 
+  ///   "MODULATE" Multiply texture color with current color. Arg1 * Arg2 
+  ///   "REPLACE" Replace current color. Arg2.
+  ///   "MODULATE2X" Multiply the components of the arguments, and shift 
+  ///   the products to the left 1 bit (effectively multiplying them by 2) 
+  ///   for brightening.
+  ///   "MODULATE4X" Multiply the components of the arguments, and shift 
+  ///   the products to the left 2 bits (effectively multiplying them by 4)
+  ///   for brightening.
+  ///   "ADD" Add the components of the arguments. Arg1 + Arg2
+  ///   "ADDSIGNED" Add the components of the arguments with a -0.5 bias,
+  ///   making the effective range of values from -0.5 through 0.5.
+  ///   "ADDSIGNED2X" Add the components of the arguments with a -0.5 bias,
+  ///   and shift the products to the left 1 bit. 
+  ///   "SUBTRACT" Subtract the components of the second argument from those
+  ///   of the first argument. Arg1 - Arg2.
+  ///   "BLENDDIFFUSEALPHA" Linearly blend this texture stage, using the 
+  ///   interpolated alpha from each vertex. Arg1 * (Alpha) + Arg2 * (1-Alpha).
+  ///   "BLENDTEXTUREALPHA" Linearly blend this texture stage, using the alpha
+  ///   from this stage's texture. Arg1 * (Alpha) + Arg2 * (1 - Alpha).
+  ///   "BLENDFACTORALPHA" Linearly blend this texture stage, using the alpha
+  ///   factor from the MultiTexture node. Arg1 × (Alpha) + Arg2 × (1 = Alpha).
+  ///   "BLENDCURRENTALPHA" Linearly blend this texture stage, using the alpha
+  ///   taken from the previous texture stage. 
+  ///   Arg1 * (Alpha)+Arg2*(1 - Alpha)
+  ///   "OFF" Turn off the texture unit
+  ///   "SELECTARG1" Use color argument 1. Arg1.
+  ///   "SELECTARG2" Use color argument 2. Arg2.
+  ///   "DOTPRODUCT3" Modulate the components of each argument (as signed 
+  ///   components), add their products, then replicate the sum to all 
+  ///   color channels, including alpha.
+  ///   This can do either diffuse or specular bump mapping with correct
+  ///   input. Performs the function 
+  ///   (Arg1.R × Arg2.R + Arg1.G × Arg2.G + Arg1.B × Arg2.B) where each
+  ///   component has been scaled and offset to make it signed. The result 
+  ///   is replicated into all four (including alpha) channels.
   /// - The function field defines an optional function to be applied to
   ///   the argument after the mode has been evaluated.
-  ///   This is not supported at this time.
+  ///   "" (default)	No function is applied.
+  ///   "COMPLEMENT" Invert the argument so that, if the result of the 
+  ///   argument were referred to by the variable x, the value would be
+  ///   1.0 minus x.
+  ///   "ALPHAREPLICATE" Replicate the alpha information to all color 
+  ///   channels before the operation completes.
   /// - The source field determines the colour source for the second argument.
-  ///   This is not supported at this time.
+  ///   "" (default) The second argument color (ARG2) is the color from the
+  ///   previous rendering stage (DIFFUSE for first stage).
+  ///   "DIFFUSE" The texture argument is the diffuse color interpolated
+  ///   from vertex components during Gouraud shading.
+  ///   "FACTOR" The texture argument is the factor (color, alpha) from
+  ///   the MultiTexture node.
   /// - The texture field contains a list of X3DTextureNode instances 
   ///   (e.g., ImageTexture, PixelTexture and MultiTexture). 
   ///   The texture field may not contain another MultiTexture node.
