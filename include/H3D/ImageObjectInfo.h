757297c5a7db5112a20853c872fd008e60a6e971
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2009-04-06 09:00:48 +0000

    Added ImageObjectInfo node
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@1209 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ include/H3D/ImageObjectInfo.h
@@ -0,0 +1,140 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file ImageObjectInfo.h
+/// \brief Header file for ImageObjectInfo.
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef __IMAGEOBJECTINFO_H__
+#define __IMAGEOBJECTINFO_H__
+
+#include <H3D/H3DImageObject.h>
+#include <H3D/X3DChildNode.h>
+#include <H3D/SFInt32.h>
+#include <H3D/SFVec3f.h>
+#include <H3D/PeriodicUpdate.h>
+#include <H3D/X3DTexture3DNode.h>
+
+namespace H3D {
+  /// \ingroup H3DNodes 
+  /// \class ImageObjectInfo
+  /// \brief This node can be used to get information about dimensions, sizes
+  /// pixel types etc from H3DImageObject nodes such as Image3DTexture.
+  class H3DAPI_API ImageObjectInfo : public X3DChildNode {
+  public:
+    
+    /// An MFNode where we make sure the type of the nodes contained
+    /// is a subclass of X3DTextureNode.
+    typedef DependentSFNode< X3DTexture3DNode, 
+                             FieldRef< H3DImageObject,
+                                       H3DImageObject::SFImage,
+                                       &H3DImageObject::image >,
+                             true >
+    SFImageObjectNode;
+
+    /// Field for updating the output fields when the image object
+    /// changes value.
+    class H3DAPI_API UpdateFields: public PeriodicUpdate< SFImageObjectNode > {
+      virtual void update();
+    };
+
+    ImageObjectInfo( Inst< SFImageObjectNode > _texture = 0,
+                     Inst< SFVec3f           > _pixelSize = 0,
+                     Inst< SFInt32           > _width = 0,
+                     Inst< SFInt32           > _height = 0,
+                     Inst< SFInt32           > _depth = 0,
+                     Inst< SFString          > _pixelType = 0,
+                     Inst< SFString          > _pixelComponentType = 0,
+                     Inst< SFInt32           > _bitsPerPixel = 0,
+                     Inst< SFVec3f           > _totalSize = 0 );
+
+    /// The texture field contains the image object we want to get 
+    /// information from.
+    ///
+    /// <b>Access type:</b> inputOutput \n
+    auto_ptr< SFImageObjectNode >  texture;
+
+    /// The size of each pixel/voxel in the image object in metres.
+    ///
+    /// <b>Access type:</b> outputOnly \n
+    auto_ptr< SFVec3f > pixelSize;
+
+    /// The width of the image object in pixels.
+    ///
+    /// <b>Access type:</b> outputOnly \n
+    auto_ptr< SFInt32 > width;
+
+    /// The height of the image object in pixels.
+    ///
+    /// <b>Access type:</b> outputOnly \n
+    auto_ptr< SFInt32 > height;
+
+    /// The depth of the image object in pixels.
+    ///
+    /// <b>Access type:</b> outputOnly \n
+    auto_ptr< SFInt32 > depth;
+
+    /// The type of each pixel element in the image object. Valid values are:
+    /// - "LUMINANCE" 
+    /// - "LUMINANCE_ALPHA"
+    /// - "RGB"
+    /// - "RGBA"
+    /// - "BGR"
+    /// - "BGRA"
+    /// - "VEC3"
+    /// 
+    /// <b>Access type:</b> outputOnly \n
+    auto_ptr< SFString > pixelType;
+
+    /// The type of each component of the pixel. Valid values are:
+    /// - "SIGNED" 
+    /// - "UNSIGNED"
+    /// - "RATIONAL"
+    /// 
+    /// <b>Access type:</b> outputOnly \n
+    auto_ptr< SFString > pixelComponentType;
+
+    /// The number of bits used for each pixel.
+    /// 
+    /// <b>Access type:</b> outputOnly \n
+    auto_ptr< SFInt32 > bitsPerPixel;
+
+    /// The total size of the volume in metres, i.e. 
+    /// Vec3f( width  * pixel_size.x, 
+    ///        height * pixel_size.y, 
+    ///        depth  * pixel_size.z )
+    ///
+    /// <b>Access type:</b> outputOnly \n
+    auto_ptr< SFVec3f > totalSize;
+
+    /// The H3DNodeDatabase for this node.
+    static H3DNodeDatabase database;
+  protected:
+    /// The field used to update the other fields.
+    auto_ptr< UpdateFields > update_fields;
+
+  };
+}
+
+#endif
---
+++ src/ImageObjectInfo.cpp
@@ -0,0 +1,157 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file ImageObjectInfo.cpp
+/// \brief CPP file for ImageObjectInfo.
+///
+//
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <H3D/ImageObjectInfo.h>
+
+using namespace H3D;
+
+// Add this node to the H3DNodeDatabase system.
+H3DNodeDatabase ImageObjectInfo::database( 
+                                       "ImageObjectInfo", 
+                                       &(newInstance<ImageObjectInfo>), 
+                                       typeid( ImageObjectInfo ),
+                                       &X3DChildNode::database );
+
+namespace ImageObjectInfoInternals {
+  FIELDDB_ELEMENT( ImageObjectInfo, texture, INPUT_OUTPUT );
+  FIELDDB_ELEMENT( ImageObjectInfo, pixelSize, OUTPUT_ONLY );
+  FIELDDB_ELEMENT( ImageObjectInfo, width, OUTPUT_ONLY );
+  FIELDDB_ELEMENT( ImageObjectInfo, height, OUTPUT_ONLY );
+  FIELDDB_ELEMENT( ImageObjectInfo, depth, OUTPUT_ONLY );
+  FIELDDB_ELEMENT( ImageObjectInfo, pixelType, OUTPUT_ONLY );
+  FIELDDB_ELEMENT( ImageObjectInfo, pixelComponentType, OUTPUT_ONLY );
+  FIELDDB_ELEMENT( ImageObjectInfo, bitsPerPixel, OUTPUT_ONLY );
+  FIELDDB_ELEMENT( ImageObjectInfo, totalSize, OUTPUT_ONLY );
+}
+
+
+ImageObjectInfo::ImageObjectInfo( Inst< SFImageObjectNode > _texture,
+                                  Inst< SFVec3f           > _pixelSize,
+                                  Inst< SFInt32           > _width,
+                                  Inst< SFInt32           > _height,
+                                  Inst< SFInt32           > _depth,
+                                  Inst< SFString          > _pixelType,
+                                  Inst< SFString          > _pixelComponentType,
+                                  Inst< SFInt32           > _bitsPerPixel,
+                                  Inst< SFVec3f           > _totalSize ):
+  texture( _texture ),
+  pixelSize( _pixelSize ),
+  width( _width ),
+  height( _height ),
+  depth( _depth ),
+  pixelType( _pixelType ),
+  pixelComponentType( _pixelComponentType ),
+  bitsPerPixel( _bitsPerPixel ),
+  totalSize( _totalSize ),
+  update_fields( new UpdateFields ) {
+
+  type_name = "ImageObjectInfo";
+  database.initFields( this );
+
+  update_fields->setOwner( this );
+
+  texture->route( update_fields );
+
+  width->setValue( 0, id );
+  height->setValue( 0, id );
+  depth->setValue( 0, id );
+  pixelType->setValue( "UNKNOWN", id );
+  pixelComponentType->setValue( "UNKNOWN", id );
+  bitsPerPixel->setValue( 0, id );
+}
+
+
+void ImageObjectInfo::UpdateFields::update() {
+  
+  ImageObjectInfo * info = static_cast< ImageObjectInfo * >( getOwner() );
+  
+  H3DImageObject *image_object = 
+    static_cast< SFImageObjectNode *>( routes_in[0] )->getValue();
+
+  Image *image = NULL;
+  if( image_object ) image = image_object->image->getValue();
+
+  if( image ) {
+    const Vec3f &pixel_size = image->pixelSize();
+    if( info->pixelSize->getValue() != pixel_size ) {
+      info->pixelSize->setValue( pixel_size, info->id );
+    }
+
+    if( info->width->getValue() != image->width() ) {
+      info->width->setValue( image->width(), info->id );
+    }
+
+    if( info->height->getValue() != image->height() ) {
+      info->height->setValue( image->height(), info->id );
+    }
+
+    if( info->depth->getValue() != image->depth() ) {
+      info->depth->setValue( image->depth(), info->id );
+    }
+
+    if( info->bitsPerPixel->getValue() != image->bitsPerPixel() ) {
+      info->bitsPerPixel->setValue( image->bitsPerPixel(), info->id );
+    }
+
+    Image::PixelType pixel_type = image->pixelType();
+    if( pixel_type == Image::LUMINANCE ) {
+      info->pixelType->setValue( "LUMINANCE", info->id );
+    } else  if( pixel_type == Image::LUMINANCE_ALPHA ) {
+      info->pixelType->setValue( "LUMINANCE_ALPHA", info->id );
+    } else  if( pixel_type == Image::RGB ) {
+      info->pixelType->setValue( "RGB", info->id );
+    } else  if( pixel_type == Image::RGBA ) {
+      info->pixelType->setValue( "RGBA", info->id );
+    } else  if( pixel_type == Image::BGR ) {
+      info->pixelType->setValue( "BGR", info->id );
+    } else  if( pixel_type == Image::BGRA ) {
+      info->pixelType->setValue( "BGRA", info->id );
+    } else if( pixel_type == Image::VEC3 ) {
+      info->pixelType->setValue( "VEC3", info->id );
+    } else {
+      info->pixelType->setValue( "UNKNOWN", info->id );
+    }
+
+    Image::PixelComponentType component_type = image->pixelComponentType();
+    if( component_type == Image::SIGNED ) {
+      info->pixelComponentType->setValue( "SIGNED", info->id );
+    } else if( component_type == Image::UNSIGNED ) {
+      info->pixelComponentType->setValue( "UNSIGNED" , info->id);
+    } else if( component_type == Image::RATIONAL ) {
+      info->pixelComponentType->setValue( "RATIONAL", info->id );
+    } else {
+      info->pixelComponentType->setValue( "UNKNOWN", info->id );
+    }
+
+    info->totalSize->setValue( Vec3f( image->width() * pixel_size.x, 
+                                      image->height() * pixel_size.y, 
+                                      image->depth() * pixel_size.z ), info->id ); 
+  }
+}
