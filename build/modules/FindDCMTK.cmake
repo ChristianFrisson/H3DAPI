91ab0f79aae47a4480b07216b27bf13cb93a9bbf
markus <markus@610dfd11-2700-0410-9b68-9ae28eac5983>
2009-04-07 10:44:17 +0000

    Updates to link properly if Dicom and teem are found.
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@1213 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ build/modules/FindDCMTK.cmake
@@ -0,0 +1,287 @@
+# - find DCMTK libraries
+#
+
+#  DCMTK_INCLUDE_DIR   - Directories to include to use DCMTK
+#  DCMTK_LIBRARIES     - Files to link against to use DCMTK
+#  DCMTK_FOUND         - If false, don't try to use DCMTK
+#  DCMTK_DIR           - (optional) Source directory for DCMTK
+#
+# DCMTK_DIR can be used to make it simpler to find the various include
+# directories and compiled libraries if you've just compiled it in the
+# source tree. Just set it to the root of the tree where you extracted
+# the source.
+#
+# Written for VXL by Amitha Perera.
+# 
+
+SET( DCMTK_DIR "" CACHE PATH "Set this to the root of the installed dcmtk files to find include files and libraries." )
+MARK_AS_ADVANCED(DCMTK_DIR)
+
+GET_FILENAME_COMPONENT(module_file_path ${CMAKE_CURRENT_LIST_FILE} PATH )
+
+SET( DCMTK_LIBRARY_POSTFIX "" )
+
+IF( MSVC80 )
+  SET( DCMTK_LIBRARY_POSTFIX "_vc8" )
+ELSEIF( MSVC90 )
+  SET( DCMTK_LIBRARY_POSTFIX "_vc9" )
+ENDIF( MSVC80 )
+
+IF( NOT WIN32 )
+SET( DCMTK_HAVE_CONFIG_H "NO" CACHE BOOL "On some systems the compile flag -DHAVE_CONFIG_H needs to be defined because of DCMTK. If DCMTK headers are causing problem turn this flag on." )
+IF( DCMTK_HAVE_CONFIG_H )
+  SET(CMAKE_CXX_FLAGS "-DHAVE_CONFIG_H")
+ENDIF( DCMTK_HAVE_CONFIG_H )
+ENDIF( NOT WIN32 )
+
+FIND_PATH( DCMTK_config_INCLUDE_DIR NAMES dcmtk/config/osconfig.h
+           PATHS ${DCMTK_DIR}/config/include 
+                 ${DCMTK_DIR}/include 
+                 $ENV{H3D_EXTERNAL_ROOT}/include
+                 $ENV{H3D_ROOT}/../External/include
+                 ../../External/include
+                 ${module_file_path}/../../../External/include
+                 ${module_file_path}/../../dcmtk/include )
+MARK_AS_ADVANCED(DCMTK_config_INCLUDE_DIR)
+
+FIND_PATH( DCMTK_ofstd_INCLUDE_DIR NAMES dcmtk/ofstd/ofstdinc.h
+           PATHS ${DCMTK_DIR}/ofstd/include 
+                 ${DCMTK_DIR}/include/ofstd
+                 ${DCMTK_DIR}/include
+                 $ENV{H3D_EXTERNAL_ROOT}/include
+                 $ENV{H3D_ROOT}/../External/include
+                 ../../External/include
+                 ${module_file_path}/../../../External/include
+                 ${module_file_path}/../../dcmtk/include )
+MARK_AS_ADVANCED(DCMTK_ofstd_INCLUDE_DIR)
+
+FIND_LIBRARY( DCMTK_ofstd_LIBRARY "ofstd${DCMTK_LIBRARY_POSTFIX}"
+              PATHS ${DCMTK_DIR}/ofstd/libsrc
+                    ${DCMTK_DIR}/ofstd/libsrc/Release
+                    ${DCMTK_DIR}/ofstd/libsrc/Debug
+                    ${DCMTK_DIR}/ofstd/Release
+                    ${DCMTK_DIR}/ofstd/Debug
+                    ${DCMTK_DIR}/lib
+                    $ENV{H3D_EXTERNAL_ROOT}/lib
+                    $ENV{H3D_ROOT}/../External/lib
+                    ../../External/lib
+                    ${module_file_path}/../../../External/lib
+                    ${module_file_path}/../../dcmtk/lib )
+MARK_AS_ADVANCED(DCMTK_ofstd_LIBRARY)
+
+FIND_PATH( DCMTK_dcmjpeg_INCLUDE_DIR NAMES dcmtk/dcmjpeg/djdecode.h
+           PATHS ${DCMTK_DIR}/dcmjpeg/include 
+                 ${DCMTK_DIR}/include/dcmjpeg
+                 ${DCMTK_DIR}/include
+                 $ENV{H3D_EXTERNAL_ROOT}/include
+                 $ENV{H3D_ROOT}/../External/include
+                 ../../External/include
+                 ${module_file_path}/../../../External/include
+                 ${module_file_path}/../../dcmtk/include )
+MARK_AS_ADVANCED(DCMTK_dcmjpeg_INCLUDE_DIR)
+
+FIND_LIBRARY( DCMTK_dcmjpeg_LIBRARY dcmjpeg${DCMTK_LIBRARY_POSTFIX}
+              PATHS ${DCMTK_DIR}/dcmjpeg/libsrc
+                    ${DCMTK_DIR}/dcmjpeg/libsrc/Release
+                    ${DCMTK_DIR}/dcmjpeg/libsrc/Debug
+                    ${DCMTK_DIR}/dcmjpeg/Release
+                    ${DCMTK_DIR}/dcmjpeg/Debug
+                    ${DCMTK_DIR}/lib
+                    $ENV{H3D_EXTERNAL_ROOT}/lib
+                    $ENV{H3D_ROOT}/../External/lib
+                    ../../External/lib
+                    ${module_file_path}/../../../External/lib
+                    ${module_file_path}/../../dcmtk/lib )
+MARK_AS_ADVANCED(DCMTK_dcmjpeg_LIBRARY)
+
+
+FIND_LIBRARY( DCMTK_ijg8_LIBRARY ijg8${DCMTK_LIBRARY_POSTFIX}
+              PATHS ${DCMTK_DIR}/dcmjpeg/libijg8/libsrc
+                    ${DCMTK_DIR}/dcmjpeg/libijg8/libsrc/Release
+                    ${DCMTK_DIR}/dcmjpeg/libijg8/libsrc/Debug
+                    ${DCMTK_DIR}/dcmjpeg/libijg8/Release
+                    ${DCMTK_DIR}/dcmjpeg/libijg8/Debug
+                    ${DCMTK_DIR}/lib
+                    $ENV{H3D_EXTERNAL_ROOT}/lib
+                    $ENV{H3D_ROOT}/../External/lib
+                    ../../External/lib
+                    ${module_file_path}/../../../External/lib
+                    ${module_file_path}/../../dcmtk/lib )
+MARK_AS_ADVANCED(DCMTK_ijg8_LIBRARY)
+
+FIND_LIBRARY( DCMTK_ijg12_LIBRARY ijg12${DCMTK_LIBRARY_POSTFIX}
+              PATHS ${DCMTK_DIR}/dcmjpeg/libijg12/libsrc
+                    ${DCMTK_DIR}/dcmjpeg/libijg12/libsrc/Release
+                    ${DCMTK_DIR}/dcmjpeg/libijg12/libsrc/Debug
+                    ${DCMTK_DIR}/dcmjpeg/libijg12/Release
+                    ${DCMTK_DIR}/dcmjpeg/libijg12/Debug
+                    ${DCMTK_DIR}/lib
+                    $ENV{H3D_EXTERNAL_ROOT}/lib
+                    $ENV{H3D_ROOT}/../External/lib
+                    ../../External/lib
+                    ${module_file_path}/../../../External/lib
+                    ${module_file_path}/../../dcmtk/lib )
+MARK_AS_ADVANCED(DCMTK_ijg12_LIBRARY)
+
+FIND_LIBRARY( DCMTK_ijg16_LIBRARY ijg16${DCMTK_LIBRARY_POSTFIX}
+              PATHS ${DCMTK_DIR}/dcmjpeg/libijg16/libsrc
+                    ${DCMTK_DIR}/dcmjpeg/libijg16/libsrc/Release
+                    ${DCMTK_DIR}/dcmjpeg/libijg16/libsrc/Debug
+                    ${DCMTK_DIR}/dcmjpeg/libijg16/Release
+                    ${DCMTK_DIR}/dcmjpeg/libijg16/Debug
+                    ${DCMTK_DIR}/lib
+                    $ENV{H3D_EXTERNAL_ROOT}/lib
+                    $ENV{H3D_ROOT}/../External/lib
+                    ../../External/lib
+                    ${module_file_path}/../../../External/lib
+                    ${module_file_path}/../../dcmtk/lib )
+MARK_AS_ADVANCED(DCMTK_ijg16_LIBRARY)
+
+FIND_PATH( DCMTK_dcmdata_INCLUDE_DIR dcmtk/dcmdata/dctypes.h
+           PATHS ${DCMTK_DIR}/dcmdata/include
+                 ${DCMTK_DIR}/include/dcmdata
+                 ${DCMTK_DIR}/include
+                 $ENV{H3D_EXTERNAL_ROOT}/include
+                 $ENV{H3D_ROOT}/../External/include
+                 ../../External/include
+                 ${module_file_path}/../../../External/include
+                 ${module_file_path}/../../dcmtk/include )
+MARK_AS_ADVANCED(DCMTK_dcmdata_INCLUDE_DIR)
+
+FIND_LIBRARY( DCMTK_dcmdata_LIBRARY dcmdata${DCMTK_LIBRARY_POSTFIX}
+              PATHS ${DCMTK_DIR}/dcmdata/libsrc
+                    ${DCMTK_DIR}/dcmdata/libsrc/Release
+                    ${DCMTK_DIR}/dcmdata/libsrc/Debug
+                    ${DCMTK_DIR}/dcmdata/Release
+                    ${DCMTK_DIR}/dcmdata/Debug
+                    ${DCMTK_DIR}/lib
+                    $ENV{H3D_EXTERNAL_ROOT}/lib
+                    $ENV{H3D_ROOT}/../External/lib
+                    ../../External/lib
+                    ${module_file_path}/../../../External/lib
+                    ${module_file_path}/../../dcmtk/lib )
+MARK_AS_ADVANCED(DCMTK_dcmdata_LIBRARY)
+
+
+FIND_PATH( DCMTK_dcmimgle_INCLUDE_DIR dcmtk/dcmimgle/dcmimage.h
+           PATHS ${DCMTK_DIR}/dcmimgle/include
+                 ${DCMTK_DIR}/include/dcmimgle
+                 ${DCMTK_DIR}/include
+                 $ENV{H3D_EXTERNAL_ROOT}/include
+                 $ENV{H3D_ROOT}/../External/include
+                 ../../External/include
+                 ${module_file_path}/../../../External/include
+                 ${module_file_path}/../../dcmtk/include )
+MARK_AS_ADVANCED(DCMTK_dcmimgle_INCLUDE_DIR)
+
+FIND_LIBRARY( DCMTK_dcmimgle_LIBRARY dcmimgle${DCMTK_LIBRARY_POSTFIX}
+              PATHS ${DCMTK_DIR}/dcmimgle/libsrc
+                    ${DCMTK_DIR}/dcmimgle/libsrc/Release
+                    ${DCMTK_DIR}/dcmimgle/libsrc/Debug
+                    ${DCMTK_DIR}/dcmimgle/Release
+                    ${DCMTK_DIR}/dcmimgle/Debug
+                    ${DCMTK_DIR}/lib
+                    $ENV{H3D_EXTERNAL_ROOT}/lib
+                    $ENV{H3D_ROOT}/../External/lib
+                    ../../External/lib
+                    ${module_file_path}/../../../External/lib
+                    ${module_file_path}/../../dcmtk/lib )
+MARK_AS_ADVANCED(DCMTK_dcmimgle_LIBRARY)
+
+FIND_LIBRARY( DCMTK_dcmimage_LIBRARY dcmimage${DCMTK_LIBRARY_POSTFIX}
+              PATHS ${DCMTK_DIR}/dcmimage/libsrc
+                    ${DCMTK_DIR}/dcmimage/libsrc/Release
+                    ${DCMTK_DIR}/dcmimage/libsrc/Debug
+                    ${DCMTK_DIR}/dcmimage/Release
+                    ${DCMTK_DIR}/dcmimage/Debug
+                    ${DCMTK_DIR}/lib
+                    $ENV{H3D_EXTERNAL_ROOT}/lib
+                    $ENV{H3D_ROOT}/../External/lib
+                    ../../External/lib
+                    ${module_file_path}/../../../External/lib
+                    ${module_file_path}/../../dcmtk/lib )
+MARK_AS_ADVANCED(DCMTK_dcmimage_LIBRARY)
+
+#FIND_LIBRARY(DCMTK_dcmnet_LIBRARY dcmnet${DCMTK_LIBRARY_POSTFIX}
+#${DCMTK_DIR}/dcmnet/libsrc/Release
+#${DCMTK_DIR}/dcmnet/libsrc/Debug
+#${DCMTK_DIR}/dcmnet/libsrc/
+#$ENV{H3D_EXTERNAL_ROOT}/lib
+#$ENV{H3D_ROOT}/../External/lib
+#../../External/lib
+#${module_file_path}/../../../External/lib
+#)
+
+
+IF( DCMTK_config_INCLUDE_DIR )
+IF( DCMTK_ofstd_INCLUDE_DIR )
+IF( DCMTK_ofstd_LIBRARY )
+IF( DCMTK_dcmdata_INCLUDE_DIR )
+IF( DCMTK_dcmdata_LIBRARY )
+IF( DCMTK_dcmimgle_INCLUDE_DIR )
+IF( DCMTK_dcmimgle_LIBRARY )
+IF( DCMTK_dcmimage_LIBRARY )
+IF( DCMTK_dcmjpeg_INCLUDE_DIR )
+IF( DCMTK_dcmjpeg_LIBRARY )
+
+  SET( DCMTK_FOUND "YES" )
+  SET( DCMTK_INCLUDE_DIR
+    ${DCMTK_config_INCLUDE_DIR}
+    ${DCMTK_ofstd_INCLUDE_DIR}
+    ${DCMTK_dcmdata_INCLUDE_DIR}
+    ${DCMTK_dcmimgle_INCLUDE_DIR}
+    ${DCMTK_dcmjpeg_INCLUDE_DIR}
+  )
+
+  SET( DCMTK_LIBRARIES
+    ${DCMTK_dcmimgle_LIBRARY}
+    ${DCMTK_dcmdata_LIBRARY}
+    ${DCMTK_ofstd_LIBRARY}
+    ${DCMTK_config_LIBRARY}
+    ${DCMTK_dcmimage_LIBRARY}
+    ${DCMTK_dcmjpeg_LIBRARY}
+    ${DCMTK_ijg8_LIBRARY}
+    ${DCMTK_ijg12_LIBRARY}
+    ${DCMTK_ijg16_LIBRARY}
+  )
+
+  IF(DCMTK_imagedb_LIBRARY)
+   SET( DCMTK_LIBRARIES
+   ${DCMTK_LIBRARIES}
+   ${DCMTK_imagedb_LIBRARY}
+   )
+  ENDIF(DCMTK_imagedb_LIBRARY)
+
+  IF(DCMTK_dcmnet_LIBRARY)
+   SET( DCMTK_LIBRARIES
+   ${DCMTK_LIBRARIES}
+   ${DCMTK_dcmnet_LIBRARY}
+   )
+  ENDIF(DCMTK_dcmnet_LIBRARY)
+
+  IF( WIN32 )
+    SET( DCMTK_LIBRARIES ${DCMTK_LIBRARIES} netapi32 ws2_32 )
+  ENDIF( WIN32 )
+
+ENDIF( DCMTK_dcmjpeg_LIBRARY )
+ENDIF( DCMTK_dcmjpeg_INCLUDE_DIR )
+ENDIF( DCMTK_dcmimage_LIBRARY )
+ENDIF( DCMTK_dcmimgle_LIBRARY )
+ENDIF( DCMTK_dcmimgle_INCLUDE_DIR )
+ENDIF( DCMTK_dcmdata_LIBRARY )
+ENDIF( DCMTK_dcmdata_INCLUDE_DIR )
+ENDIF( DCMTK_ofstd_LIBRARY )
+ENDIF( DCMTK_ofstd_INCLUDE_DIR )
+ENDIF( DCMTK_config_INCLUDE_DIR )
+
+# Report the results.
+IF(NOT DCMTK_FOUND)
+  SET(DCMTK_DIR_MESSAGE
+    "DCMTK was not found. Make sure DCMTK_LIBRARY and DCMTK_INCLUDE_DIR are set to where you have your dcmtk header and lib files. If you do not have the library you will not be able to use the DicomImageLoader node.")
+  IF(DCMTK_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "${DCMTK_DIR_MESSAGE}")
+  ELSEIF(NOT DCMTK_FIND_QUIETLY)
+    MESSAGE(STATUS "${DCMTK_DIR_MESSAGE}")
+  ENDIF(DCMTK_FIND_REQUIRED)
+ENDIF(NOT DCMTK_FOUND)
+
---
+++ build/modules/FindTEEM.cmake
@@ -0,0 +1,175 @@
+# - Find TEEM
+# Find the native TEEM headers and libraries.
+#
+#  TEEM_INCLUDE_DIR -  where to find TEEM.h, etc.
+#  TEEM_LIBRARIES    - List of libraries when using TEEM.
+#  TEEM_FOUND        - True if TEEM found.
+#  
+#  In the future when the teem library fixes their CMakeLists.txt so that
+#  the moss library is installed correctly then it is possible to rename
+#  this module into FindTeemH3D (or something) and use the syntax below
+#  to try to find TEEM. The CMakeLists.txt for MedX3D would have to be changed
+#  to call the new module name instead to avoid conflicts.
+#  FIND_PATH(TEEM_DIR NAMES TEEMUse.cmake
+#                   PATHS /usr/local/lib/TEEM-1.9 )
+#  MARK_AS_ADVANCED(TEEM_DIR)
+#
+#  IF( TEEM_DIR )
+#    FIND_PACKAGE( TEEM )
+#    IF( TEEM_FOUND )
+#      INCLUDE( ${TEEM_USE_FILE} )
+#    ELSE( TEEM_FOUND )
+#      #Try to find libraries like the code below.
+#    ENDIF( TEEM_FOUND
+#  ENDIF( TEEM_DIR )
+#
+#
+#
+
+IF( NOT WIN32 AND UNIX )
+  SET( TEEM_BZIP2 "NO" CACHE BOOL "If teem is built with bz2 support then this variable should be set to ON." )
+  SET( TEEM_PNG "NO" CACHE BOOL "If teem is built with png support then this variable should be set to ON." )
+  SET( TEEM_ZLIB "NO" CACHE BOOL "If teem is built with zlib support then this variable should be set to ON." )
+ELSE(  NOT WIN32 AND UNIX )
+  SET( TEEM_BZIP2 "YES" CACHE BOOL "If teem is built with bz2 support then this variable should be set to ON." )
+  SET( TEEM_PNG "YES" CACHE BOOL "If teem is built with png support then this variable should be set to ON." )
+  SET( TEEM_ZLIB "YES" CACHE BOOL "If teem is built with zlib support then this variable should be set to ON." )
+ENDIF( NOT WIN32 AND UNIX )
+MARK_AS_ADVANCED( TEEM_BZIP2 )
+MARK_AS_ADVANCED( TEEM_PNG )
+MARK_AS_ADVANCED( TEEM_ZLIB )
+
+GET_FILENAME_COMPONENT(module_file_path ${CMAKE_CURRENT_LIST_FILE} PATH )
+
+# Look for the header file.
+FIND_PATH(TEEM_INCLUDE_DIR NAMES teem/nrrd.h
+                           PATHS $ENV{H3D_EXTERNAL_ROOT}/include
+                                 $ENV{H3D_ROOT}/../External/include
+                                 ../../External/include
+                                 /usr/local/include
+                                 ${module_file_path}/../../../External/include
+                                 ${module_file_path}/../../teem/include )
+MARK_AS_ADVANCED(TEEM_INCLUDE_DIR)
+
+# Look for the library.
+FIND_LIBRARY(TEEM_LIBRARY NAMES nrrd teem 
+                          PATHS $ENV{H3D_EXTERNAL_ROOT}/lib
+                                $ENV{H3D_ROOT}/../External/lib
+                                ../../External/lib
+                                /usr/local/lib
+                                ${module_file_path}/../../../External/lib
+                                ${module_file_path}/../../teem/lib )
+MARK_AS_ADVANCED(TEEM_LIBRARY)
+
+IF( TEEM_LIBRARY )
+  SET( FOUND_TEEM_LIBRARIES 1 )
+ELSE( TEEM_LIBRARY )
+  SET( FOUND_TEEM_LIBRARIES 0 )
+ENDIF( TEEM_LIBRARY )
+
+IF( TEEM_ZLIB )
+  FIND_LIBRARY(Z_LIBRARY NAMES z
+                         PATHS $ENV{H3D_EXTERNAL_ROOT}/lib
+                               $ENV{H3D_ROOT}/../External/lib
+                               ../../External/lib
+                               /usr/lib
+                               ${module_file_path}/../../../External/lib
+                               ${module_file_path}/../../teem/lib )
+  MARK_AS_ADVANCED(Z_LIBRARY)
+  IF( NOT Z_LIBRARY )
+    SET( FOUND_TEEM_LIBRARIES 0 )
+  ENDIF( NOT Z_LIBRARY )
+ENDIF( TEEM_ZLIB )
+
+IF( TEEM_PNG )
+  FIND_LIBRARY(PNG_LIBRARY NAMES png
+                           PATHS $ENV{H3D_EXTERNAL_ROOT}/lib
+                                 $ENV{H3D_ROOT}/../External/lib
+                                  ../../External/lib
+                                  /usr/lib
+                                  ${module_file_path}/../../../External/lib
+                                  ${module_file_path}/../../teem/lib )
+  MARK_AS_ADVANCED(PNG_LIBRARY)
+  IF( NOT PNG_LIBRARY )
+    SET( FOUND_TEEM_LIBRARIES 0 )
+  ENDIF( NOT PNG_LIBRARY )
+ENDIF( TEEM_PNG )
+
+IF( TEEM_BZIP2 )
+  FIND_LIBRARY(BZ2_LIBRARY NAMES bz2
+                           PATHS $ENV{H3D_EXTERNAL_ROOT}/lib
+                                 $ENV{H3D_ROOT}/../External/lib
+                                 ../../External/lib
+                                 /usr/lib
+                                 ${module_file_path}/../../../External/lib
+                                 ${module_file_path}/../../teem/lib )
+  MARK_AS_ADVANCED(BZ2_LIBRARY)
+  IF( NOT BZ2_LIBRARY )
+    SET( FOUND_TEEM_LIBRARIES 0 )
+  ENDIF( NOT BZ2_LIBRARY )
+ENDIF( TEEM_BZIP2 )
+
+IF( NOT WIN32 AND UNIX )
+  FIND_LIBRARY(AIR_LIBRARY NAMES air
+                           PATHS $ENV{H3D_EXTERNAL_ROOT}/lib
+                                 $ENV{H3D_ROOT}/../External/lib
+                                 ../../External/lib
+                                 ${module_file_path}/../../../External/lib
+                                 /usr/lib
+                                 ${module_file_path}/../../teem/lib )
+  MARK_AS_ADVANCED(AIR_LIBRARY)
+  IF( NOT AIR_LIBRARY )
+    SET( FOUND_TEEM_LIBRARIES 0 )
+  ENDIF( NOT AIR_LIBRARY )
+
+  FIND_LIBRARY(BIFF_LIBRARY NAMES biff
+                            PATHS $ENV{H3D_EXTERNAL_ROOT}/lib
+                                  $ENV{H3D_ROOT}/../External/lib
+                                  ../../External/lib
+                                  ${module_file_path}/../../../External/lib
+                                  /usr/lib
+                                  ${module_file_path}/../../teem/lib )
+  MARK_AS_ADVANCED(BIFF_LIBRARY)
+  IF( NOT BIFF_LIBRARY )
+    SET( FOUND_TEEM_LIBRARIES 0 )
+  ENDIF( NOT BIFF_LIBRARY )
+ENDIF( NOT WIN32 AND UNIX )
+
+# Copy the results to the output variables.
+IF(TEEM_INCLUDE_DIR AND FOUND_TEEM_LIBRARIES)
+  SET(TEEM_FOUND 1)
+  SET(TEEM_LIBRARIES ${TEEM_LIBRARY})
+
+  IF( Z_LIBRARY )
+    SET(TEEM_LIBRARIES ${TEEM_LIBRARIES} ${Z_LIBRARY})
+  ENDIF( Z_LIBRARY )
+  IF( PNG_LIBRARY )
+    SET(TEEM_LIBRARIES ${TEEM_LIBRARIES} ${PNG_LIBRARY})
+  ENDIF( PNG_LIBRARY )
+  IF( BZ2_LIBRARY )
+    SET(TEEM_LIBRARIES ${TEEM_LIBRARIES} ${BZ2_LIBRARY})
+  ENDIF( BZ2_LIBRARY )
+  IF( AIR_LIBRARY )
+    SET(TEEM_LIBRARIES ${TEEM_LIBRARIES} ${AIR_LIBRARY})
+  ENDIF( AIR_LIBRARY )
+  IF( BIFF_LIBRARY )
+    SET(TEEM_LIBRARIES ${TEEM_LIBRARIES} ${BIFF_LIBRARY})
+  ENDIF( BIFF_LIBRARY )
+
+  SET(TEEM_INCLUDE_DIR ${TEEM_INCLUDE_DIR})
+ELSE(TEEM_INCLUDE_DIR AND FOUND_TEEM_LIBRARIES)
+  SET(TEEM_FOUND 0)
+  SET(TEEM_LIBRARIES)
+  SET(TEEM_INCLUDE_DIR)
+ENDIF(TEEM_INCLUDE_DIR AND FOUND_TEEM_LIBRARIES)
+
+# Report the results.
+IF(NOT TEEM_FOUND)
+  SET(TEEM_DIR_MESSAGE
+    "TEEM was not found. Make sure TEEM_LIBRARY, PNG_LIBRARY, Z_LIBRARY, BZ2_LIBRARY and TEEM_INCLUDE_DIR are set to where you have your teem header and different lib files. If you do not have the libraries you will not be able to use the NrrdImageLoader node. Some of the libraries only need to be set if teem should have support for those features.")
+  IF(TEEM_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "${TEEM_DIR_MESSAGE}")
+  ELSEIF(NOT TEEM_FIND_QUIETLY)
+    MESSAGE(STATUS "${TEEM_DIR_MESSAGE}")
+  ENDIF(TEEM_FIND_REQUIRED)
+ENDIF(NOT TEEM_FOUND)
