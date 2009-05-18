6878e7a30d12e702ebcfde2d04e56056106cc48e
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2009-05-18 08:08:18 +0000

    Added OSX H3DViewer CPack files
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@1259 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ H3DViewer/build/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+H3DVIEWER_BUNDLE="`echo "$0" | sed -e 's/\/Contents\/MacOS\/H3DViewer//'`"
+
+H3DVIEWER_RESOURCES="$H3DVIEWER_BUNDLE/Contents/Resources"
+
+echo "running $0"
+echo "H3DVIEWER_BUNDLE: $H3DVIEWER_BUNDLE"
+
+export "DYLD_LIBRARY_PATH=$H3DVIEWER_RESOURCES/lib"
+#export "FONTCONFIG_PATH=$H3DVIEWER_RESOURCES/etc/fonts"
+
+#export
+exec "$H3DVIEWER_RESOURCES/bin/H3DViewer"
