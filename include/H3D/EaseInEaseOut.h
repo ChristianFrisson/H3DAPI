a0d383470aaef07748ddc5c71fedf5739d13427b
juliana <juliana@610dfd11-2700-0410-9b68-9ae28eac5983>
2009-06-23 14:17:49 +0000

    Implemented easeInEaseOut node. X3D example included.
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@1307 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ include/H3D/EaseInEaseOut.h
@@ -0,0 +1,143 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file EaseInEaseOut.h
+/// \brief Header file for EaseInEaseOut, X3D scene-graph node
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef __EASEINEASEOUT_H__
+#define __EASEINEASEOUT_H__
+
+#include <H3D/X3DChildNode.h>
+#include <H3D/SFFloat.h>
+#include <H3D/MFFloat.h>
+#include <H3D/MFVec2f.h>
+
+namespace H3D {
+
+  /// \ingroup X3DNodes
+  /// \class EaseInEaseOut
+  /// \brief The EaseInEaseOut node supports controlled gradual 
+  /// transitions by specifying modifications for TimeSensor node 
+  /// fractions. The EaseInEaseOut node receives set_fraction field
+  /// event. It uses the values of the key field and the easeIneaseOut
+  /// field to modify that fraction which is then issued as a 
+  /// modifiedFraction_changed event.
+  ///
+  /// The easeInEaseOut field shall have the same size as the key
+  /// field, and each Vec2f value of the easeInEaseOut field 
+  /// corresponds to the key value with the same index. The first 
+  /// component of each easeInEaseOut field Vec2f value corresponds 
+  /// to the easeIn feature as the interpolator approaches the key, 
+  /// and the second component of each easeInEaseOut field Vec2f 
+  /// value corresponds to the easeOut feature as the interpolator 
+  /// progresses away from the key.
+  ///
+  /// The values of the easeInEaseOut field range from zero to one. 
+  /// At zero, there is no modification of the fraction.
+  ///
+  /// The scope of the easeOut effect on the local fraction is equal 
+  /// to the easeOut value.
+  ///
+  /// \par Internal routes:
+  /// \dotfile EaseInEaseOut.dot
+
+  class H3DAPI_API EaseInEaseOut : public X3DChildNode {
+  public:
+
+    /// Thrown when number of field values in key and easeInEaseOut
+    /// are not equal
+    H3D_API_EXCEPTION( UnequalFieldValues );
+
+    /// Class for modified fraction_changed field type
+    ///
+    /// routes_in[0] is the set_fraction field
+    /// routes_in[1] is the key field
+    /// routes_in[2] is the easeInEaseOutField
+    class H3DAPI_API SFFraction : public TypedField< SFFloat, 
+                              Types<SFFloat, MFFloat, MFVec2f> > {
+    protected:
+      virtual void update();
+    };
+
+    /// Constructor.
+    EaseInEaseOut( Inst< SFFloat    > _set_fraction    = 0,
+		               Inst< MFVec2f    > _easeInEaseOut = 0,
+                   Inst< MFFloat    > _key = 0,
+                   Inst< SFNode     > _metadata = 0,
+                   Inst< SFFraction > _modifiedFraction_changed = 0 );
+
+    /// Incoming fraction field values routed from TimeSensor node.
+    /// 
+    /// <b>Access type:</b> inputOnly
+    /// 
+    /// \dotfile EaseInEaseOut_set_fraction.dot
+    auto_ptr< SFFloat > set_fraction;
+
+    /// The proportion of time at which ease-in and -out effect should
+    /// take place for the corresponding key value. The first component 
+    /// of this Vec2f field value corresponds to the easeIn feature as 
+    /// the interpolator approaches the key. The second component of 
+    /// corresponds to the easeOut feature as the interpolator progresses 
+    /// away from the key. The range of value is 0.0 to 1.0. The number 
+    /// of field values must be same as that of the key field.
+    /// 
+    /// <b>Access type:</b> inputOutput
+    /// <b>Default value:</b> []
+    /// 
+    /// \dotfile EaseInEaseOut_easeInEaseOut.dot
+    auto_ptr< MFVec2f > easeInEaseOut;
+
+    /// The keypoints for interpolation.
+    /// 
+    /// <b>Access type:</b> inputOutput
+    /// <b>Default value:</b> []
+    /// 
+    /// \dotfile EaseInEaseOut_key.dot
+    auto_ptr< MFFloat > key;
+
+    /// Output fraction as modified from specified key and 
+    /// easeInEaseOut values.
+    /// 
+    /// <b>Access type:</b> outputOnly
+    /// 
+    /// \dotfile EaseInEaseOut_modifiedFraction_changed.dot
+    auto_ptr< SFFloat > modifiedFraction_changed;
+
+    /// The H3DNodeDatabase for this node.
+    static H3DNodeDatabase database;
+
+  protected:
+    /// Utility function to find the index for the key pair 
+    /// for a given fraction value f. The proportion of distance
+    /// between the key pair at f is set in p.
+    int lookupKey( H3DFloat f, H3DFloat &p );
+
+    /// Check that easeInEaseOut and key have the same number of
+    /// field values, and print error message if they are not equal.
+    void checkKeyEaseInEaseOut();
+  };
+}
+
+#endif
---
+++ src/EaseInEaseOut.cpp
@@ -0,0 +1,141 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file EaseInEaseOut.cpp
+/// \brief CPP file for EaseInEaseOut, X3D scene-graph node
+///
+//
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <H3D/EaseInEaseOut.h>
+
+using namespace H3D;
+
+
+// Add this node to the H3DNodeDatabase system.
+H3DNodeDatabase EaseInEaseOut::database( 
+                           "EaseInEaseOut", 
+                           &( newInstance<EaseInEaseOut> ), 
+                           typeid( EaseInEaseOut ),
+                           &X3DChildNode::database );
+
+namespace EaseInEaseOutInternals {
+  FIELDDB_ELEMENT( EaseInEaseOut, set_fraction, INPUT_ONLY );
+  FIELDDB_ELEMENT( EaseInEaseOut, easeInEaseOut, INPUT_OUTPUT );
+  FIELDDB_ELEMENT( EaseInEaseOut, key, INPUT_OUTPUT );
+  FIELDDB_ELEMENT( EaseInEaseOut, modifiedFraction_changed, OUTPUT_ONLY );
+}
+
+EaseInEaseOut::EaseInEaseOut( 
+                         Inst< SFFloat    > _set_fraction,
+                         Inst< MFVec2f    > _easeInEaseOut,
+                         Inst< MFFloat    > _key,
+                         Inst< SFNode     > _metadata,
+                         Inst< SFFraction > _modifiedFraction_changed ) :
+  X3DChildNode( _metadata ),
+  set_fraction( _set_fraction ),
+  easeInEaseOut( _easeInEaseOut ),
+  key( _key ),
+  modifiedFraction_changed( _modifiedFraction_changed ) {
+  
+  type_name = "EaseInEaseOut";
+  database.initFields( this );
+
+  set_fraction->route( modifiedFraction_changed, id );
+  key->route( modifiedFraction_changed, id );
+  easeInEaseOut->route( modifiedFraction_changed, id );
+}
+
+int EaseInEaseOut::lookupKey( H3DFloat f, H3DFloat &p ) {
+  vector<H3DFloat> keys = key->getValue();
+  if( keys.size() == 0 ) return -1;
+  if ( keys.size() == 1 || f <= keys[0] ) {
+    p = 0;
+    return 0;
+  }
+  if ( f >= keys[keys.size()-1] ) {
+    p = 1;
+    return (int)keys.size() - 2;
+  }
+  for( size_t i = keys.size()-1; i >= 0; i--) {
+    if ( f > keys[i] ) {
+      p = (f-keys[i])/(keys[i+1]-keys[i]);
+      if( p < 0 ) p = 0;
+      if( p > 1 ) p = 1;
+      return (int) i;
+    }
+  }
+  return -1;
+}
+
+void EaseInEaseOut::checkKeyEaseInEaseOut() {
+  if ( key->size() != easeInEaseOut->size() ) {
+    stringstream s;
+    s << "Number of field values in key and easeInEaseOut "
+      << "must be the same (in \"" << getName() << "\" node) ";
+    throw UnequalFieldValues( s.str(), H3D_FULL_LOCATION );
+  }
+}
+
+void EaseInEaseOut::SFFraction::update() {
+  EaseInEaseOut * e = static_cast< EaseInEaseOut* >( getOwner() );
+  // Check that number of values in easeInEaseOut and key are equal
+  e->checkKeyEaseInEaseOut();
+  H3DFloat fraction 
+    = static_cast< SFFloat* >(routes_in[0])->getValue(e->id);
+  vector< H3DFloat > key 
+    = static_cast< MFFloat* >(routes_in[1])->getValue();
+  vector< Vec2f > ease 
+    = static_cast< MFVec2f* >(routes_in[2])->getValue();
+  
+  // No keys, return
+  if ( key.size() == 0 ) return;
+
+  // Calculations as specified in X3D specification
+  H3DFloat u, eout, ein;
+  int key_index = e->lookupKey( fraction, u );
+
+  eout = ease.at( key_index ).y;
+  ein = ease.at( (key_index+1)%key.size() ).x;
+  
+  H3DFloat sum, t = 0;
+  sum = ein + eout;
+
+  if ( sum < 0 ) {
+    value = u;
+  }
+  if ( sum > 1.0 ) {
+    ein /= sum;
+    eout /= sum;
+  }
+  t = 1.0f / ( 2-eout-ein );
+  if ( u < eout ) {
+    value = ( t/eout ) * u * u;
+  } else if ( u < (1.0-ein) ) {
+    value = t * ( 2*u-eout );
+  } else {
+    value = 1.0f - ( (t*(1-u)*(1-u))/ein );
+  }
+}
+
