ec56b60832769ceda372d59e48715c503f7d236e
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2010-05-28 10:25:26 +0000

    Updated shadow caster so that there is not just one shadow level(in or out of shadow) but several levels if several lightsources are specified.
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@1572 610dfd11-2700-0410-9b68-9ae28eac5983

--- src/ShadowCaster.cpp
+++ src/ShadowCaster.cpp
@@ -72,85 +72,90 @@ ShadowCaster::ShadowCaster(
 }
 
 void ShadowCaster::render() {
-  
   if( X3DShapeNode::geometry_render_mode == X3DShapeNode::SOLID ||
       X3DShapeNode::geometry_render_mode == X3DShapeNode::TRANSPARENT_BACK ||
       object->empty() || light->empty() ) return;
   X3DChildNode::render();
-  
-  glClear(GL_STENCIL_BUFFER_BIT );
-  glPushAttrib( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | 
-                GL_ENABLE_BIT | GL_POLYGON_BIT | GL_STENCIL_BUFFER_BIT );
-  glDisable( GL_LIGHTING );// Turn Off Lighting
-  glDepthMask( GL_FALSE );// Turn Off Writing To The Depth-Buffer
-  glDepthFunc( GL_LESS );
-  glEnable( GL_STENCIL_TEST );// Turn On Stencil Buffer Testing
-  glColorMask( GL_FALSE, GL_FALSE, 
-               GL_FALSE, GL_FALSE );// Don't Draw Into The Colour Buffer
-  glEnable( GL_CULL_FACE );
-  
-  const string &alg = algorithm->getValue();
-  glStencilFunc( GL_ALWAYS, 1, 0xFFFFFFFFL );
 
-  if( alg == "ZPASS" ) {
-    glStencilOp( GL_KEEP, GL_KEEP, GL_INCR );
-    glCullFace( GL_BACK );
-  } else {
-    glStencilOp(GL_KEEP, GL_INCR, GL_KEEP);
-    glCullFace(GL_FRONT);
-  }
+  // set the darkness so that if a point is in shadow from all lights,
+  // the darkness should be shadowDarkness.
+  // Basically the solution to (1-x)^nr_lights = (1-shadowDarkness)
+  H3DFloat darkness = 1 - H3DPow( 1 - shadowDarkness->getValue(), 1.0/light->size() );
+  
+  for( MFLightNode::const_iterator l = light->begin(); 
+       l != light->end(); l++ ) {
 
-  // First Pass. Increase Stencil Value In The Shadow 
-  for( MFShadowObjectNode::const_iterator o = object->begin(); 
-       o != object->end(); o++ ) {
-    for( MFLightNode::const_iterator l = light->begin(); 
-         l != light->end(); l++ ) {
+    glClear(GL_STENCIL_BUFFER_BIT );
+    glPushAttrib( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | 
+                  GL_ENABLE_BIT | GL_POLYGON_BIT | GL_STENCIL_BUFFER_BIT );
+    glDisable( GL_LIGHTING );// Turn Off Lighting
+    glDepthMask( GL_FALSE );// Turn Off Writing To The Depth-Buffer
+    glDepthFunc( GL_LESS );
+    glDisable( GL_ALPHA_TEST );
+    glEnable( GL_STENCIL_TEST );// Turn On Stencil Buffer Testing
+    glColorMask( GL_FALSE, GL_FALSE, 
+                 GL_FALSE, GL_FALSE );// Don't Draw Into The Colour Buffer
+    glEnable( GL_CULL_FACE );
+    
+    const string &alg = algorithm->getValue();
+    glStencilFunc( GL_ALWAYS, 1, 0xFFFFFFFFL );
+    
+    if( alg == "ZPASS" ) {
+      glStencilOp( GL_KEEP, GL_KEEP, GL_INCR );
+      glCullFace( GL_BACK );
+    } else {
+      glStencilOp(GL_KEEP, GL_INCR, GL_KEEP);
+      glCullFace(GL_FRONT);
+    }
+
+    // First Pass. Increase Stencil Value In The Shadow 
+    for( MFShadowObjectNode::const_iterator o = object->begin(); 
+	 o != object->end(); o++ ) {
+
       static_cast< H3DShadowObjectNode * >(*o)->renderShadow( static_cast< X3DLightNode * >(*l), alg == "ZFAIL" );
     }
-  }
-
-  // Second Pass. Decrease Stencil Value In The Shadow
-  if( alg == "ZPASS" ) {
-    glStencilOp( GL_KEEP, GL_KEEP, GL_DECR );
-    glCullFace( GL_FRONT );
-  } else {
-    glStencilOp(GL_KEEP, GL_DECR, GL_KEEP);
-		glCullFace(GL_BACK);
-  }
   
-  for( MFShadowObjectNode::const_iterator o = object->begin(); 
-       o != object->end(); o++ ) {
-    for( MFLightNode::const_iterator l = light->begin(); l != light->end(); l++ ) {
+    // Second Pass. Decrease Stencil Value In The Shadow
+    if( alg == "ZPASS" ) {
+      glStencilOp( GL_KEEP, GL_KEEP, GL_DECR );
+      glCullFace( GL_FRONT );
+    } else {
+      glStencilOp(GL_KEEP, GL_DECR, GL_KEEP);
+      glCullFace(GL_BACK);
+    }
+    
+    for( MFShadowObjectNode::const_iterator o = object->begin(); 
+	 o != object->end(); o++ ) {
       static_cast< H3DShadowObjectNode * >(*o)->renderShadow( static_cast< X3DLightNode * >(*l), alg == "ZFAIL" );
     }
+
+    // Enable Rendering To Colour Buffer For All Components
+    glColorMask( GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE );
+    
+    // Draw A Shadowing Rectangle Covering The Entire Screen
+    glColor4f( 0.0f, 0.0f, 0.0f, darkness );
+    glEnable( GL_BLEND );
+    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+    glStencilFunc( GL_NOTEQUAL, 0, 0xFFFFFFFFL );
+    glStencilOp( GL_KEEP, GL_KEEP, GL_KEEP );
+    glDisable( GL_DEPTH_TEST );
+    glDisable( GL_CULL_FACE );
+    glMatrixMode (GL_MODELVIEW);
+    glPushMatrix ();
+    glLoadIdentity ();
+    glMatrixMode (GL_PROJECTION);
+    glPushMatrix ();
+    glLoadIdentity ();
+    glBegin (GL_QUADS);
+    glVertex3i (-1, -1, -1);
+    glVertex3i (1, -1, -1);
+    glVertex3i (1, 1, -1);
+    glVertex3i (-1, 1, -1);
+    glEnd ();
+    glPopMatrix ();
+    glMatrixMode (GL_MODELVIEW);
+    glPopMatrix ();
+    glPopAttrib(); 
   }
 
-  // Enable Rendering To Colour Buffer For All Components
-  glColorMask( GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE );
-  
-  // Draw A Shadowing Rectangle Covering The Entire Screen
-  glColor4f( 0.0f, 0.0f, 0.0f, shadowDarkness->getValue() );
-  glEnable( GL_BLEND );
-  glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
-  glStencilFunc( GL_NOTEQUAL, 0, 0xFFFFFFFFL );
-  glStencilOp( GL_KEEP, GL_KEEP, GL_KEEP );
-  glDisable( GL_DEPTH_TEST );
-  glDisable( GL_CULL_FACE );
-  glMatrixMode (GL_MODELVIEW);
-  glPushMatrix ();
-  glLoadIdentity ();
-  glMatrixMode (GL_PROJECTION);
-  glPushMatrix ();
-  glLoadIdentity ();
-  glBegin (GL_QUADS);
-  glVertex3i (-1, -1, -1);
-  glVertex3i (1, -1, -1);
-  glVertex3i (1, 1, -1);
-  glVertex3i (-1, 1, -1);
-  glEnd ();
-  glPopMatrix ();
-  glMatrixMode (GL_MODELVIEW);
-  glPopMatrix ();
-
-  glPopAttrib(); 
 }
