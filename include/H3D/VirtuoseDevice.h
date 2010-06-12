84366c0e4a322dba7335aedc76f6f5ad571b8b48
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2010-06-12 22:08:03 +0000

    Added VirtuoseDevice
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@1584 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ include/H3D/VirtuoseDevice.h
@@ -0,0 +1,85 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file VirtuoseDevice.h
+/// \brief Header file for VirtuoseDevice.
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef __VIRTUOSEDEVICE_H__
+#define __VIRTUOSEDEVICE_H__
+
+#include <H3D/H3DHapticsDevice.h>
+#include <H3D/SFString.h>
+
+namespace H3D {
+
+  /// \ingroup H3DNodes
+  /// \class VirtuoseDevice
+  /// \brief A VirtuoseDevice is a node for handling communication
+  /// with a haptics device from Haption, such as the Virtuose haptics device.
+  class H3DAPI_API VirtuoseDevice: public H3DHapticsDevice {
+  public:
+
+    /// Constructor.
+    VirtuoseDevice( 
+            Inst< SFVec3f            > _devicePosition         = 0,
+            Inst< SFRotation         > _deviceOrientation      = 0,
+            Inst< TrackerPosition    > _trackerPosition        = 0,
+            Inst< TrackerOrientation > _trackerOrientation     = 0,
+            Inst< PosCalibration     > _positionCalibration    = 0,
+            Inst< OrnCalibration     > _orientationCalibration = 0,
+            Inst< SFVec3f            > _proxyPosition          = 0,
+            Inst< WeightedProxy      > _weightedProxyPosition  = 0,
+            Inst< SFFloat            > _proxyWeighting         = 0,
+            Inst< SFBool             > _mainButton             = 0,
+            Inst< SFBool             > _secondaryButton        = 0,
+            Inst< SFInt32            > _buttons                = 0,
+            Inst< SFVec3f            > _force                  = 0,
+            Inst< SFVec3f            > _torque                 = 0,
+            Inst< SFInt32            > _inputDOF               = 0,
+            Inst< SFInt32            > _outputDOF              = 0,
+            Inst< SFInt32            > _hapticsRate            = 0,
+            Inst< SFInt32            > _desiredHapticsRate     = 0,
+            Inst< SFNode             > _stylus                 = 0,
+            Inst< SFHapticsRendererNode > _hapticsRenderer     = 0,
+            Inst< MFVec3f            > _proxyPositions         = 0,
+            Inst< SFBool             > _followViewpoint        = 0,
+            Inst< SFString           > _ipAddress              = 0 );
+    
+    /// Creates a VirtuoseHapticsDevice in the hapi_device
+    /// with name deviceName
+    virtual void initialize();
+
+    /// The ip address of the Virtuose device.
+    ///
+    /// <b>Access type:</b> initializeOnly \n
+    /// <b>Default value:</b> "192.168.1.1" \n
+    auto_ptr< SFString > ipAddress;
+
+    /// Node database entry
+    static H3DNodeDatabase database;
+  };
+}
+
+#endif
---
+++ src/VirtuoseDevice.cpp
@@ -0,0 +1,99 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file VirtuoseDevice.cpp
+/// \brief cpp file for VirtuoseDevice
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+#include <H3D/VirtuoseDevice.h>
+#include <HAPI/HaptionHapticsDevice.h>
+
+using namespace H3D;
+
+H3DNodeDatabase VirtuoseDevice::database( "VirtuoseDevice", 
+                                          &(newInstance<VirtuoseDevice>),
+                                          typeid( VirtuoseDevice ),
+                                          &H3DHapticsDevice::database ); 
+namespace VirtuoseDeviceInternals {
+  FIELDDB_ELEMENT( VirtuoseDevice, ipAddress, INITIALIZE_ONLY );
+}
+
+/// Constructor.
+VirtuoseDevice::VirtuoseDevice( 
+               Inst< SFVec3f         > _devicePosition,
+               Inst< SFRotation      > _deviceOrientation,
+               Inst< TrackerPosition > _trackerPosition,
+               Inst< TrackerOrientation > _trackerOrientation,
+               Inst< PosCalibration  > _positionCalibration,
+               Inst< OrnCalibration  > _orientationCalibration,
+               Inst< SFVec3f         > _proxyPosition,
+               Inst< WeightedProxy   > _weightedProxyPosition,     
+               Inst< SFFloat         > _proxyWeighting,
+               Inst< SFBool          > _mainButton,
+               Inst< SFBool          > _secondaryButton,
+               Inst< SFInt32         > _buttons,
+               Inst< SFVec3f         > _force,
+               Inst< SFVec3f         > _torque,
+               Inst< SFInt32         > _inputDOF,
+               Inst< SFInt32         > _outputDOF,
+               Inst< SFInt32         > _hapticsRate,
+               Inst< SFInt32         > _desiredHapticsRate,
+               Inst< SFNode          > _stylus,
+               Inst< SFHapticsRendererNode > _hapticsRenderer,
+               Inst< MFVec3f         > _proxyPositions,
+               Inst< SFBool          > _followViewpoint,
+               Inst< SFString        > _ipAddress ) :
+  H3DHapticsDevice( _devicePosition, _deviceOrientation, _trackerPosition,
+                    _trackerOrientation, _positionCalibration, 
+                    _orientationCalibration, _proxyPosition,
+                    _weightedProxyPosition, _proxyWeighting, _mainButton,
+                    _secondaryButton, _buttons,_force, _torque, _inputDOF,
+                    _outputDOF, _hapticsRate, _desiredHapticsRate, _stylus,
+                    _hapticsRenderer, _proxyPositions, _followViewpoint ),
+  ipAddress( _ipAddress ) {
+
+  type_name = "VirtuoseDevice";  
+  database.initFields( this );
+  
+  hapi_device.reset(0);
+  
+  ipAddress->setValue( "192.168.1.1" );
+}
+
+void VirtuoseDevice::initialize() {
+  H3DHapticsDevice::initialize();
+  const string &ip_address = ipAddress->getValue();
+
+#ifdef HAVE_VIRTUOSEAPI
+  hapi_device.reset( new HAPI::HaptionHapticsDevice( ip_address ) );
+#else
+  Console(4) << "Cannot use VirtuoseDevice. HAPI compiled without"
+             << " VirtuoseAPI support. Recompile HAPI with "
+             << "HAVE_VIRTUOSEAPI defined"
+             << " in order to use it." << endl;
+#endif // HAVE_VIRTUOSEAPI
+} 
+
