d25331496455ce14cb80d6008cb64c881480e37f
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2010-11-05 15:04:35 +0000

    Added RenderTargetSelectGroup node and example
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@1675 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ include/H3D/RenderTargetSelectGroup.h
@@ -0,0 +1,93 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2010, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file RenderTargetSelectGroup.h
+/// \brief Header file for RenderTargetSelectGroup.
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef __RENDERTARGETSELECTGROUP_H__
+#define __RENDERTARGETSELECTGROUP_H__
+
+#include <H3D/X3DGroupingNode.h>
+#include <H3D/MFInt32.h>
+
+namespace H3D {
+
+  /// \ingroup X3DNodes
+  /// \class RenderTargetSelectGroup
+  /// \brief The RenderTargetSelectGroup is used in combination with the 
+  /// FrameBufferTextureGenerator to select which color textures to render into.
+  ///
+  /// The RenderTargetSelectGroup must always be a child of a FrameBufferTextureGenerator.
+  /// If used outside a FrameBufferTextureGenerator the result is undefined.
+  ///
+  /// The renderTargets field contains the indices of the color textures of the parent
+  /// FrameBufferTextureGenerator you want to render the children into. If an index provided
+  /// is outside the range of the parent FrameBufferTextureGenerator color textures the 
+  /// result is undefined.
+  ///
+  /// <b>Examples:</b>
+  ///   - <a href="../../../H3DAPI/examples/All/RenderTargetSelectGroup.x3d">RenderTargetSelectGroup.x3d</a>
+  ///     ( <a href="examples/RenderTargetSelectGroup.x3d.html">Source</a> )
+  class H3DAPI_API RenderTargetSelectGroup : public X3DGroupingNode {
+  public:
+    /// Constructor.
+    RenderTargetSelectGroup ( Inst< AddChildren    > _addChildren    = 0,
+			      Inst< RemoveChildren > _removeChildren = 0,
+			      Inst< MFChild        > _children       = 0,
+			      Inst< SFNode         > _metadata       = 0,
+			      Inst< SFBound        > _bound          = 0,
+			      Inst< SFVec3f        > _bboxCenter     = 0,
+			      Inst< SFVec3f        > _bboxSize       = 0,
+			      Inst< MFInt32        > _renderTargets  = 0);
+
+    virtual void render();
+
+    /// The renderTargets field contains the indices of the color textures of the parent
+    /// FrameBufferTextureGenerator you want to render the children into. If an index provided
+    /// is outside the range of the parent FrameBufferTextureGenerator color textures the 
+    /// result is undefined.
+    ///
+    /// <b>Access type:</b> inputOutput
+    /// <b>Default value:</b> []
+    auto_ptr< MFInt32 > renderTargets;
+
+    /// The H3DNodeDatabase for this node.
+    static H3DNodeDatabase database;
+
+  protected:
+    /// Internal buffer for storing OpenGL enumerators for use with
+    /// the glDrawBuffers call.
+    auto_ptr< GLenum > draw_buffers;
+
+    /// The maximum number of draw buffers supported by the graphics card. 
+    int max_nr_draw_buffers;
+
+    /// Flag used to avoid printing more than one error message when the graphics
+    /// card does not support the extensions needed for this node to work.
+    bool support_message_displayed;
+  };
+}
+
+#endif
---
+++ src/RenderTargetSelectGroup.cpp
@@ -0,0 +1,134 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file RenderTargetSelectGroup.cpp
+/// \brief CPP file for RenderTargetSelectGroup.
+///
+//
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <H3D/RenderTargetSelectGroup.h>
+
+using namespace H3D;
+
+// Add this node to the H3DNodeDatabase system.
+H3DNodeDatabase RenderTargetSelectGroup::database( "RenderTargetSelectGroup", 
+                                 &(newInstance<RenderTargetSelectGroup>), 
+                                 typeid( RenderTargetSelectGroup ),
+                                 &X3DGroupingNode::database );
+
+namespace RenderTargetSelectGroupInternals {
+  FIELDDB_ELEMENT( RenderTargetSelectGroup, renderTargets, INPUT_OUTPUT );
+}
+
+
+RenderTargetSelectGroup::RenderTargetSelectGroup( 
+             Inst< AddChildren    > _addChildren,
+             Inst< RemoveChildren > _removeChildren,
+             Inst< MFChild        > _children,
+             Inst< SFNode         > _metadata,
+             Inst< SFBound        > _bound,
+             Inst< SFVec3f        > _bboxCenter,
+             Inst< SFVec3f        > _bboxSize,
+	     Inst< MFInt32        > _renderTargets ) :
+  X3DGroupingNode( _addChildren, _removeChildren, _children, 
+		   _metadata, _bound, _bboxCenter, _bboxSize ),
+  renderTargets( _renderTargets ),
+  support_message_displayed( false ) {
+
+  type_name = "RenderTargetSelectGroup";
+  database.initFields( this );
+  
+}
+
+
+void RenderTargetSelectGroup::render() {
+
+  // Check that we have hardware support
+  if( !GLEW_EXT_framebuffer_object ) {
+    if( !support_message_displayed ) {
+      Console(4) << "Warning: Frame Buffer Objects not supported by your graphics card "
+		 << "(EXT_frame_buffer_object). RenderTargetSelectGroup nodes have no effect. " 
+		 << endl;
+      support_message_displayed = true;
+    }
+    return;
+  }
+
+  if( !GLEW_ARB_draw_buffers ) {
+    if( !support_message_displayed ) {
+      Console(4) << "Warning: Your graphics card does not support multiple "
+		 << "render targets(ARB_draw_buffers). RenderTargetSelectGroup nodes have no effect. " << endl;
+      support_message_displayed = true;
+    }
+    return;
+  }
+
+  glPushAttrib( GL_COLOR_BUFFER_BIT);
+ 
+  const vector< H3DInt32 > &render_targets = renderTargets->getValue();
+
+  // initialize draw buffers array.
+  if( !draw_buffers.get() ) {
+    GLint max_draw_buffers, max_color_attachments;
+    glGetIntegerv( GL_MAX_DRAW_BUFFERS_ARB, &max_draw_buffers );
+    glGetIntegerv( GL_MAX_COLOR_ATTACHMENTS_EXT, &max_color_attachments );
+    max_nr_draw_buffers = H3DMin( max_draw_buffers, max_color_attachments );
+    draw_buffers.reset( new GLenum[max_nr_draw_buffers] );
+  }
+
+  if( render_targets.size() > 0 ) {
+    // construct current draw buffers array
+
+    // reset all draw buffers to NONE.
+    for( size_t i = 0; i < max_nr_draw_buffers; i++ ) {
+      draw_buffers.get()[i] = GL_NONE;
+    }
+
+    // set the specified draw buffers to the color attachement specified by renderTargets.
+    int draw_buffer_size = 0;
+    for( size_t i = 0; i < render_targets.size(); i++ ) {
+      int target = render_targets[i];
+      if( target >= 0 && target < max_nr_draw_buffers ) {
+	draw_buffers.get()[target] = GL_COLOR_ATTACHMENT0_EXT + target;
+	if( target >= draw_buffer_size ) draw_buffer_size = target + 1;
+      } else {
+	Console(4) << "Warning: Invalid render target: " << target 
+		   << ". Targets supported by your graphics card are between 0 and "
+		   << max_nr_draw_buffers - 1 << "( in RenderTargetSelectGroup). " << endl;
+      }
+
+    }
+
+    // change draw buffers.
+    glDrawBuffersARB( draw_buffer_size, draw_buffers.get() );
+  } else {
+    // no targets specified, set to draw to no color buffer.
+    glDrawBuffer( GL_NONE );
+  }
+
+  X3DGroupingNode::render();
+
+  glPopAttrib();
+}
