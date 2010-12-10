581f4f0b22bad8578796ee1d22fab9336a3833f1
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2010-12-10 08:20:48 +0000

    Added EntactDevice
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@1692 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ include/H3D/EntactDevice.h
@@ -0,0 +1,117 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file EntactDevice.h
+/// \brief Header file for EntactDevice.
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef __ENTACTDEVICE_H__
+#define __ENTACTDEVICE_H__
+
+#include <H3D/H3DHapticsDevice.h>
+#include <H3D/MFString.h>
+
+namespace H3D {
+
+  /// \ingroup H3DNodes
+  /// \class EntactDevice
+  /// \brief A EntactDevice is a node for handling communication
+  /// with a haptics device from Entact.
+  ///
+  /// <b>Examples:</b>
+  ///   - <a href="../../../H3DAPI/examples/All/EntactDevice.x3d">EntactDevice.x3d</a>
+  ///     ( <a href="examples/EntactDevice.x3d.html">Source</a> )
+  class H3DAPI_API EntactDevice: public H3DHapticsDevice {
+  public:
+
+    /// Field class that calibrates the device when a true event is received
+    /// or the field is set to true.
+    class H3DAPI_API Calibrate: public AutoUpdate< OnValueChangeSField< SFBool > > {
+      virtual void onValueChange( const bool &value );
+    };
+
+    /// Constructor.
+    EntactDevice( 
+            Inst< SFVec3f            > _devicePosition         = 0,
+            Inst< SFRotation         > _deviceOrientation      = 0,
+            Inst< TrackerPosition    > _trackerPosition        = 0,
+            Inst< TrackerOrientation > _trackerOrientation     = 0,
+            Inst< PosCalibration     > _positionCalibration    = 0,
+            Inst< OrnCalibration     > _orientationCalibration = 0,
+            Inst< SFVec3f            > _proxyPosition          = 0,
+            Inst< WeightedProxy      > _weightedProxyPosition  = 0,     
+            Inst< SFFloat            > _proxyWeighting         = 0,
+            Inst< SFBool             > _mainButton            = 0,
+            Inst< SFBool             > _secondaryButton       = 0,
+            Inst< SFInt32            > _buttons                = 0,
+            Inst< SFVec3f            > _force                  = 0,
+            Inst< SFVec3f            > _torque                 = 0,
+            Inst< SFInt32            > _inputDOF               = 0,
+            Inst< SFInt32            > _outputDOF              = 0,
+            Inst< SFInt32            > _hapticsRate            = 0,
+            Inst< SFInt32            > _desiredHapticsRate     = 0,
+            Inst< SFNode             > _stylus                 = 0,
+            Inst< SFHapticsRendererNode > _hapticsRenderer     = 0,
+            Inst< MFVec3f            > _proxyPositions         = 0,
+            Inst< SFBool             > _followViewpoint        = 0,
+            Inst< SFInt32            > _serialNumber           = 0,
+            Inst< SFBool             > _needsCalibration       = 0,
+            Inst< Calibrate          > _calibrate              = 0 );
+
+    /// Creates a EntactHapticsDevice in the hapi_device
+    /// member with the specified serial number.
+    virtual void initialize();
+
+    /// updateDeviceValues is specialized to set the needsCalibration field
+    /// if needed.
+    virtual void updateDeviceValues();
+
+    /// The serial number of the device to use. If specified only the
+    /// device with the specified serial number can be initialized
+    /// by the node. If -1 any Entact device that is connected to 
+    /// the computer can be used.
+    /// 
+    /// <b>Default value:</b> -1 \n 
+    /// <b>Access type:</b> initializeOnly \n 
+    auto_ptr< SFInt32  > serialNumber;
+
+    /// This field is true if the device needs to be calibrated.
+    /// 
+    /// <b>Access type:</b> outputOnly \n   
+    auto_ptr< SFBool > needsCalibration;
+
+    /// When a true event is received, the calibration procedure for the 
+    /// device is started. The calibration procedure depends on the 
+    /// device type.
+    /// 
+    /// <b>Access type:</b> inputOnly \n   
+    auto_ptr< Calibrate > calibrate;
+
+    
+    /// Node database entry
+    static H3DNodeDatabase database;
+  };
+}
+
+#endif
---
+++ src/EntactDevice.cpp
@@ -0,0 +1,129 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file EntactDevice.cpp
+/// \brief cpp file for EntactDevice
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+#include <H3D/EntactDevice.h>
+#ifdef HAVE_ENTACTAPI
+#include <HAPI/EntactHapticsDevice.h>
+#endif
+
+using namespace H3D;
+
+H3DNodeDatabase EntactDevice::database( "EntactDevice", 
+                                        &(newInstance<EntactDevice>),
+                                        typeid( EntactDevice ),
+                                        &H3DHapticsDevice::database ); 
+namespace EntactDeviceInternals {
+  FIELDDB_ELEMENT( EntactDevice, needsCalibration, OUTPUT_ONLY );
+  FIELDDB_ELEMENT( EntactDevice, calibrate, INPUT_ONLY );
+  FIELDDB_ELEMENT( EntactDevice, serialNumber, INITIALIZE_ONLY );
+}
+
+/// Constructor.
+EntactDevice::EntactDevice( 
+               Inst< SFVec3f         > _devicePosition,
+               Inst< SFRotation      > _deviceOrientation,
+               Inst< TrackerPosition > _trackerPosition,
+               Inst< TrackerOrientation > _trackerOrientation,
+               Inst< PosCalibration  > _positionCalibration,
+               Inst< OrnCalibration  > _orientationCalibration,
+               Inst< SFVec3f         > _proxyPosition,
+               Inst< WeightedProxy   > _weightedProxyPosition,     
+               Inst< SFFloat         > _proxyWeighting,
+               Inst< SFBool          > _mainButton,
+               Inst< SFBool          > _secondaryButton,
+               Inst< SFInt32         > _buttons,
+               Inst< SFVec3f         > _force,
+               Inst< SFVec3f         > _torque,
+               Inst< SFInt32         > _inputDOF,
+               Inst< SFInt32         > _outputDOF,
+               Inst< SFInt32         > _hapticsRate,
+               Inst< SFInt32         > _desiredHapticsRate,
+               Inst< SFNode          > _stylus,
+               Inst< SFHapticsRendererNode > _hapticsRenderer,
+               Inst< MFVec3f         > _proxyPositions,
+               Inst< SFBool          > _followViewpoint,
+	       Inst< SFInt32            > _serialNumber,
+	       Inst< SFBool             > _needsCalibration,
+	       Inst< Calibrate          > _calibrate ) :
+  H3DHapticsDevice( _devicePosition, _deviceOrientation, _trackerPosition,
+              _trackerOrientation, _positionCalibration, 
+              _orientationCalibration, _proxyPosition,
+              _weightedProxyPosition, _proxyWeighting, _mainButton,
+                    _secondaryButton, _buttons,
+              _force, _torque, _inputDOF, _outputDOF, _hapticsRate,
+              _desiredHapticsRate, _stylus,_hapticsRenderer, _proxyPositions,
+	      _followViewpoint ),
+  serialNumber( _serialNumber ),
+  needsCalibration( _needsCalibration ),
+  calibrate( _calibrate ) {
+
+  type_name = "EntactDevice";  
+  database.initFields( this );
+
+  serialNumber->setValue( -1, id );
+  needsCalibration->setValue( false, id );
+}
+
+void EntactDevice::initialize() {
+#ifdef HAVE_ENTACTAPI
+  hapi_device.reset( new HAPI::EntactHapticsDevice( serialNumber->getValue() ) );
+#else
+  Console(4) << "Cannot use EntactDevice. HAPI compiled without"
+             << " Entact support. Recompile HAPI with HAVE_ENTACTAPI defined"
+             << " in order to use it." << endl;
+#endif
+}
+
+void EntactDevice::updateDeviceValues() {
+#ifdef HAVE_ENTACTAPI
+  H3DHapticsDevice::updateDeviceValues();
+  HAPI::EntactHapticsDevice *ed = 
+    dynamic_cast< HAPI::EntactHapticsDevice *>(hapi_device.get() );
+  if( ed ) {
+    bool b = ed->needsCalibration();
+    if( needsCalibration->getValue() != b ) {
+      needsCalibration->setValue( b, id );
+    }
+  }
+#endif
+}
+
+
+
+
+void EntactDevice::Calibrate::onValueChange( const bool &value ) {
+  if ( value ) {
+    EntactDevice *h3d_device = static_cast< EntactDevice * >( getOwner() );
+    HAPI::EntactHapticsDevice *hapi_device = static_cast< HAPI::EntactHapticsDevice * >( h3d_device->hapi_device.get() );
+    if( hapi_device ) {
+      hapi_device->calibrateDevice();
+    }
+  }
+}
