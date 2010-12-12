badb5cb71acbd2f2e3c017cec57407860b23bc5a
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2010-12-12 17:11:08 +0000

    Added EnvironmentMapShader node
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@1694 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ include/H3D/EnvironmentMapShader.h
@@ -0,0 +1,133 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file EnvironmentMapShader.h
+/// \brief Header file for EnvironmentMapShader.
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef __ENVIRONMENTMAPSHADER_H__
+#define __ENVIRONMENTMAPSHADER_H__
+
+#include <H3D/H3DGeneratedFragmentShaderNode.h>
+#include <H3D/X3DTexture2DNode.h>
+#include <H3D/X3DEnvironmentTextureNode.h>
+#include <H3D/SFMatrix4f.h>
+
+namespace H3D {
+
+  /// \ingroup H3DNodes
+  /// \class EnvironmentMapShader
+  /// \brief The EnvironmentMapShader node defines a shader for environment
+  /// mapping of shiny objects. 
+  ///
+  /// The environmentMap field contains a X3DEnvironmentTextureNode containing the 
+  /// environment map to use.
+  /// 
+  /// The fresnel field specifies if the fresnel effect should be used in the
+  /// reflection model and if so how much. The fresnel effect is the effect
+  /// that surfaces reflect more light if looking at them from an angle than
+  /// straight down along the normal. If set to 0 nor fresnel effect is used
+  /// and the surface is fully reflective. If set to 1 only the reflectivity
+  /// of the fresnel effect is used, which means no reflectivity when looking
+  /// down perpendicular to the surace. One can think of the term as controlling
+  /// the minimum reflectivness of the surface. By default it is set to 0 and
+  /// no fresnel effect is used. 
+  ///
+  /// <b>Examples:</b>
+  ///   - <a href="../../../H3DAPI/examples/All/EnvironmentMapShader.x3d">EnvironmentMapShader.x3d</a>
+  ///     ( <a href="examples/EnvironmentMapShader.x3d.html">Source</a> )
+  ///
+  /// \par Internal routes:
+  /// \dotfile EnvironmentMapShader.dot
+  class EnvironmentMapShader : 
+    public H3DGeneratedFragmentShaderNode {
+  public:
+
+    /// The SFEnvironmentTextureNode field is dependent on the displayList field
+    /// of the containing X3DEnvironmentTextureNode node.
+    typedef DependentSFNode< X3DEnvironmentTextureNode, 
+                             FieldRef< H3DDisplayListObject,
+                                       H3DDisplayListObject::DisplayList,
+                                       &H3DDisplayListObject::displayList >, 
+                             true >
+    SFEnvironmentTextureNode;
+
+    /// Constructor.
+    EnvironmentMapShader( Inst< DisplayList  > _displayList = 0,
+                          Inst< SFNode       > _metadata    = 0,
+                          Inst< SFBool       > _isSelected  = 0,
+                          Inst< SFBool       > _isValid     = 0,
+                          Inst< SFBool       > _activate    = 0,
+                          Inst< SFString     > _language    = 0,
+                          Inst< MFShaderPart > _parts       = 0,
+                          Inst< SFBool       > _suppressUniformWarnings = 0,
+                          Inst< MFString     > _fragmentShaderString = 0,
+                          Inst< MFString     > _vertexShaderString = 0,
+                          Inst< SFEnvironmentTextureNode > _environmentMap = 0,
+                          Inst< SFFloat                  > _fresnel = 0 );
+    
+    /// Contains the X3DEnvironmentTextureNode to use for environment mapping.
+    ///
+    /// <b>Access type:</b> inputOutput \n
+    /// <b>Default value:</b> NULL \n
+    /// 
+    /// \dotfile EnvironmentMapShader_environmentMap.dot
+    auto_ptr< SFEnvironmentTextureNode > environmentMap;
+
+    /// The fresnel field specifies if the fresnel effect should be used in the
+    /// reflection model and if so how much. The fresnel effect is the effect
+    /// that surfaces reflect more light if looking at them from an angle than
+    /// straight down along the normal. If set to 0 nor fresnel effect is used
+    /// and the surface is fully reflective. If set to 1 only the reflectivity
+    /// of the fresnel effect is used, which means no reflectivity when looking
+    /// down perpendicular to the surace. One can think of the term as controlling
+    /// the minimum reflectivness of the surface. By default it is set to 0 and
+    /// no fresnel effect is used.
+    ///
+    /// <b>Access type:</b> inputOutput \n
+    /// <b>Default value:</b> 0 \n
+    /// 
+    /// \dotfile EnvironmentMapShader_fresnel.dot
+    auto_ptr< SFFloat > fresnel;
+
+    /// The H3DNodeDatabase for this node.
+    static H3DNodeDatabase database;
+
+  protected:
+    /// Adds uniform fields to the shader. A ComposedShader uses its
+    /// dynamic fields to define uniform variables. Hence we need to
+    /// add dynamic fields for each field that we want to be accessable
+    /// in the shader.
+    virtual string addUniformFields( ComposedShader *shader );
+
+    /// Get the varying variables used by the shader generator.
+    virtual void getVaryingVariables( vector< VaryingVariable > &variables );
+
+    /// Returns the shader code for the fragment shader.
+    virtual string getFragmentShaderString();
+
+  };
+}
+
+#endif
---
+++ src/EnvironmentMapShader.cpp
@@ -0,0 +1,146 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2007, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file EnvironmentMapShader.cpp
+/// \brief CPP file for EnvironmentMapShader.
+///
+//
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <H3D/EnvironmentMapShader.h>
+#include <H3D/ShaderPart.h>
+
+using namespace H3D;
+
+// Add this node to the H3DNodeDatabase system.
+H3DNodeDatabase EnvironmentMapShader::database( 
+                                   "EnvironmentMapShader", 
+                                   &(newInstance<EnvironmentMapShader>), 
+                                   typeid( EnvironmentMapShader ),
+                                   &H3DGeneratedFragmentShaderNode::database );
+
+namespace EnvironmentMapShaderInternals {
+  FIELDDB_ELEMENT( EnvironmentMapShader, environmentMap, INPUT_OUTPUT );
+  FIELDDB_ELEMENT( EnvironmentMapShader, fresnel, INPUT_OUTPUT );
+}
+
+EnvironmentMapShader::EnvironmentMapShader( Inst< DisplayList  > _displayList,
+                                            Inst< SFNode       > _metadata,
+                                            Inst< SFBool       > _isSelected ,
+                                            Inst< SFBool       > _isValid,
+                                            Inst< SFBool       > _activate,
+                                            Inst< SFString     > _language,
+                                            Inst< MFShaderPart > _parts,
+                                            Inst< SFBool       > _suppressUniformWarnings,
+                                            Inst< MFString > _fragmentShaderString,
+                                            Inst< MFString > _vertexShaderString,
+                                            Inst< SFEnvironmentTextureNode > _environmentMap,
+                                            Inst< SFFloat      > _fresnel ) :
+  H3DGeneratedFragmentShaderNode( _displayList, _metadata, _isSelected, 
+                                  _isValid, _activate, _language, _parts, 
+                                  _suppressUniformWarnings, _fragmentShaderString,
+                                  _vertexShaderString ),
+
+  environmentMap( _environmentMap ),
+  fresnel( _fresnel ) {
+  type_name = "EnvironmentMapShader";
+  database.initFields( this );
+
+  fresnel->setValue( 0 );
+
+  environmentMap->route( displayList, id );
+  fresnel->route( displayList, id );
+
+  fresnel->route( rebuildShader, id );
+}
+
+/// Get the varying variables used by the shader generator.
+void EnvironmentMapShader::getVaryingVariables( vector< VaryingVariable > &varyings ) {
+  if( environmentMap->getValue() ) {
+    varyings.push_back( VaryingVariable( uniqueShaderName( "normal_local" ),
+                                         "vec3",
+                                         uniqueShaderName( "normal_local" ) + " = gl_Normal;" ) );
+    varyings.push_back( VaryingVariable( uniqueShaderName( "vertex_local" ),
+                                         "vec3",
+                                         uniqueShaderName( "vertex_local" ) + " = gl_Vertex;" ) );
+    varyings.push_back( VaryingVariable( uniqueShaderName( "eye_pos_local" ),
+                                         "vec3",
+                                         "vec4 camera_local = gl_ModelViewMatrixInverse * vec4( 0.0, 0.0, 0.0, 1.0); \n"
+                                         "camera_local.xyz /= camera_local.w; \n" + 
+                                         uniqueShaderName( "eye_pos_local" ) + " = camera_local;\n" ) );
+  }
+}
+
+
+string EnvironmentMapShader::getFragmentShaderString() {
+    
+  stringstream s;
+  
+  if( environmentMap->getValue() ) {
+    s << "    vec3 normal_local = normalize(" << uniqueShaderName( "normal_local" ) << ");" << endl;
+    s << "    vec3 eye_pos_local = " << uniqueShaderName( "eye_pos_local" ) << ";" << endl;
+    s << "    vec3 vertex_local = " << uniqueShaderName( "vertex_local" ) << ";" << endl;
+    s << "    vec3 eye_vec_local = normalize(vertex_local-eye_pos_local); " << endl;    
+
+    s << "    vec4 env_color = textureCube( " << uniqueShaderName( "environment_map" ) 
+      << ", reflect( eye_vec_local, normal_local)  ); " << endl;
+
+    H3DFloat r = fresnel->getValue();
+    if( r > (H3DFloat) 0.0 ) {
+      // calculate fresnel term
+      s << "    float R0 = 1.0 - " << uniqueShaderName( "fresnel" ) << ";" << endl;
+      s << "    float fresnel = R0 + (1.0-R0) * pow(1.0-abs(dot(-eye_vec_local, normal_local)), 5.0); " << endl;
+      s << "    generated_color = env_color * fresnel + generated_color * (1.0-fresnel ); " << endl;
+    } else {
+      // full reflectivity
+      s << "   generated_color = env_color; " << endl;
+    }
+  }
+  return s.str();
+}
+
+
+string EnvironmentMapShader::addUniformFields( ComposedShader *shader ) {
+  // add dynamic fields for uniform variables 
+  stringstream s;
+  
+  if( environmentMap->getValue() ) {
+    s << addUniformToFragmentShader( shader,
+                                     uniqueShaderName( "environment_map" ), 
+                                     "samplerCube",
+                                     H3D::Field::INPUT_OUTPUT,
+                                     copyAndRouteField( environmentMap ) );
+    if( fresnel->getValue() > 0.0 ) {
+      s << addUniformToFragmentShader( shader,
+                                       uniqueShaderName( "fresnel" ), 
+                                       "float",
+                                       H3D::Field::INPUT_OUTPUT,
+                                       copyAndRouteField( fresnel ) );
+    }
+    
+  }
+  
+  return s.str();
+}
+
