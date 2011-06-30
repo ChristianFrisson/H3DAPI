44874df38e1630dae33a61379966335eea3467dc
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2011-06-30 09:41:28 +0000

    Added OpenGL matrices to output fields of TransformInfo
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@1799 610dfd11-2700-0410-9b68-9ae28eac5983

--- include/H3D/TransformInfo.h
+++ include/H3D/TransformInfo.h
@@ -31,6 +31,7 @@
 
 #include <H3D/X3DChildNode.h>
 #include <H3D/SFMatrix4f.h>
+#include <H3D/H3DDisplayListObject.h>
 
 namespace H3D {
 
@@ -46,19 +47,55 @@ namespace H3D {
   /// global coordinate space to the coordinate space this Node resides
   /// in in the scenegraph.
   ///
+  /// The outputGLMatrices field determines if the OpenGL matrix fields 
+  /// should be updated or not(the fields starting with gl). 
+  /// Note that when this is true graphics caching will disabled for this
+  /// node making all parent nodes not being cached. This is most of the
+  /// time a non-issue though.
+  ///
+  /// The glModelViewMatrix is The OpenGL GL_MODELVIEW_MATRIX at the 
+  /// current point of the transform hierarchy.
+  ///
+  /// The glModelViewMatrixInverse field is the inverse matrix of 
+  /// glModelViewMatrix.
+  ///
+  /// The glProjectionMatrix is The OpenGL GL_PROJECTION_MATRIX at the 
+  /// current point of the transform hierarchy.
+  ///
+  /// The glProjectionMatrixInverse field is the inverse matrix of 
+  /// glProjectionMatrix.
+  ///
   /// <b>Examples:</b>
   ///   - <a href="../../../H3DAPI/examples/All/TransformInfo.x3d">TransformInfo.x3d</a>
   ///     ( <a href="examples/TransformInfo.x3d.html">Source</a> )
   class H3DAPI_API TransformInfo : 
-    public X3DChildNode {
+    public X3DChildNode,
+    public H3DDisplayListObject {
   public:
+
+    class H3DAPI_API Matrix4fInverse: public SFMatrix4f {
+      virtual void update() {
+        const Matrix4f &m = 
+          static_cast< SFMatrix4f* >(event.ptr)->getValue();
+        value = m.inverse();
+      }
+    };
+
     /// Constructor.
-    TransformInfo( Inst< SFNode     > _metadata  = 0,
-                   Inst< SFMatrix4f > _accForwardMatrix = 0,
-                   Inst< SFMatrix4f > _accInverseMatrix = 0 );
+    TransformInfo( Inst< SFNode      > _metadata  = 0,
+                   Inst< SFMatrix4f  > _accForwardMatrix = 0,
+                   Inst< SFMatrix4f  > _accInverseMatrix = 0,
+                   Inst< DisplayList > _displayList    = 0,
+                   Inst< SFBool      > _outputGLMatrices = 0,
+                   Inst< SFMatrix4f  > _glModelViewMatrix = 0,
+                   Inst< Matrix4fInverse > _glModelViewMatrixInverse = 0,
+                   Inst< SFMatrix4f  > _glProjectionMatrix = 0,
+                   Inst< Matrix4fInverse > _glProjectionMatrixInverse = 0 );
 
     /// Set the matrix fields from the values provided in ti.
     virtual void traverseSG( TraverseInfo &ti );
+
+  virtual void render();
 
     /// The accForwardMatrix field is the matrix that transforms from the local
     /// coordinate space where this Node resides in the scenegraph to 
@@ -74,6 +111,37 @@ namespace H3D {
     /// <b>Access type:</b> outputOnly \n
     auto_ptr< SFMatrix4f >  accInverseMatrix;
 
+    /// Determines if the OpenGL matrix fields should be updated or not
+    /// (the fields starting with gl). Note that when this is true 
+    /// graphics caching will disabled for this node making all parent
+    /// nodes not being cached. This is most of the time a non-issue though.
+    /// 
+    /// <b>Access type:</b> inputOutput \n
+    /// <b>Default value:</b> false \n
+    auto_ptr< SFBool > outputGLMatrices;
+
+    /// The OpenGL GL_MODELVIEW_MATRIX at the current point of the 
+    /// transform hierarchy.
+    /// 
+    /// <b>Access type:</b> outputOnly \n
+    auto_ptr< SFMatrix4f >  glModelViewMatrix;
+
+    /// The inverse matrix of glModelViewMatrix
+    /// 
+    /// <b>Access type:</b> outputOnly \n
+    auto_ptr< Matrix4fInverse >  glModelViewMatrixInverse;
+
+    /// The OpenGL GL_PROJECTION_MATRIX at the current point of the 
+    /// transform hierarchy.
+    /// 
+    /// <b>Access type:</b> outputOnly \n
+    auto_ptr< SFMatrix4f >  glProjectionMatrix;
+
+    /// The inverse matrix of glProjectionMatrix
+    /// 
+    /// <b>Access type:</b> outputOnly \n
+    auto_ptr< Matrix4fInverse >  glProjectionMatrixInverse;
+    
     /// The H3DNodeDatabase for this node.
     static H3DNodeDatabase database;
   };
--- src/TransformInfo.cpp
+++ src/TransformInfo.cpp
@@ -42,17 +42,45 @@ H3DNodeDatabase TransformInfo::database(
 namespace TransformInfoInternals {
   FIELDDB_ELEMENT( TransformInfo, accForwardMatrix, OUTPUT_ONLY );
   FIELDDB_ELEMENT( TransformInfo, accInverseMatrix, OUTPUT_ONLY );
+  FIELDDB_ELEMENT( TransformInfo, outputGLMatrices, INPUT_OUTPUT );
+  FIELDDB_ELEMENT( TransformInfo, glModelViewMatrix, OUTPUT_ONLY );
+  FIELDDB_ELEMENT( TransformInfo, glModelViewMatrixInverse, OUTPUT_ONLY );
+  FIELDDB_ELEMENT( TransformInfo, glProjectionMatrix, OUTPUT_ONLY );
+  FIELDDB_ELEMENT( TransformInfo, glProjectionMatrixInverse, OUTPUT_ONLY );
 }
 
 TransformInfo::TransformInfo( Inst< SFNode     > _metadata,
                               Inst< SFMatrix4f > _accForwardMatrix,
-                              Inst< SFMatrix4f > _accInverseMatrix ) :
+                              Inst< SFMatrix4f > _accInverseMatrix,
+                              Inst< DisplayList > _displayList,
+                              Inst< SFBool      > _outputGLMatrices,
+                              Inst< SFMatrix4f  > _glModelViewMatrix,
+                              Inst< Matrix4fInverse > _glModelViewMatrixInverse,
+                              Inst< SFMatrix4f  > _glProjectionMatrix,
+                              Inst< Matrix4fInverse > _glProjectionMatrixInverse ) :
   X3DChildNode( _metadata ),
+  H3DDisplayListObject( _displayList ),
   accForwardMatrix( _accForwardMatrix ),
-  accInverseMatrix( _accInverseMatrix ) {
+  accInverseMatrix( _accInverseMatrix ),
+  outputGLMatrices( _outputGLMatrices ),
+  glModelViewMatrix( _glModelViewMatrix ),
+  glModelViewMatrixInverse( _glModelViewMatrixInverse ),
+  glProjectionMatrix( _glProjectionMatrix ),
+  glProjectionMatrixInverse( _glProjectionMatrixInverse ) {
 
   type_name = "TransformInfo";
   database.initFields( this );
+
+  displayList->setOwner( this );
+
+  outputGLMatrices->setValue( false );
+
+  glModelViewMatrix->route( glModelViewMatrixInverse, id );
+  glProjectionMatrix->route( glProjectionMatrixInverse, id );
+
+  outputGLMatrices->route( displayList );
+  glModelViewMatrix->route( displayList );
+  glProjectionMatrix->route( displayList );
 }
 
 void TransformInfo::traverseSG( TraverseInfo &ti ) {
@@ -66,4 +94,33 @@ void TransformInfo::traverseSG( TraverseInfo &ti ) {
 
   if( acc_inv != accInverseMatrix->getValue() )
     accInverseMatrix->setValue( acc_inv, id );
+}
+
+void TransformInfo::render() {
+  if( outputGLMatrices->getValue() ) {
+    displayList->setCacheMode( DisplayList::OFF );
+    GLfloat mv[16], p[16];
+    glGetFloatv( GL_MODELVIEW_MATRIX, mv );
+    glGetFloatv( GL_PROJECTION_MATRIX, p );
+
+    Matrix4f model_view( mv[0], mv[4], mv[8], mv[12],
+                         mv[1], mv[5], mv[9], mv[13],
+                         mv[2], mv[6], mv[10], mv[14],
+                         mv[3], mv[7], mv[11], mv[15] );
+
+    Matrix4f projection( p[0], p[4], p[8], p[12],
+                         p[1], p[5], p[9], p[13],
+                         p[2], p[6], p[10], p[14],
+                         p[3], p[7], p[11], p[15] );
+
+    if( glModelViewMatrix->getValue() != model_view ) {
+      glModelViewMatrix->setValue( model_view, id );
+    }
+
+    if( glProjectionMatrix->getValue() != projection ) {
+      glProjectionMatrix->setValue( projection, id );
+    }
+  } else {
+    displayList->setCacheMode( DisplayList::ON );
+  }
 }
