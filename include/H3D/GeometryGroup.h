0cc8160eeac09e0f9219d19c1388b4a307eabc16
markus <markus@610dfd11-2700-0410-9b68-9ae28eac5983>
2013-11-01 11:05:52 +0000

    Added new node, GeometryGroup which can be used to let two geometries build one geometry, for example to specify simplified shadows or haptic geometries.
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@2462 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ include/H3D/GeometryGroup.h
@@ -0,0 +1,199 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2013, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file GeometryGroup.h
+/// \brief Header file for GeometryGroup, X3D scene-graph node
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef __GEOMETRYGROUP_H__
+#define __GEOMETRYGROUP_H__
+
+#include <H3D/X3DGeometryNode.h>
+#include <H3D/X3DBoundedObject.h>
+#include <H3D/DependentNodeFields.h>
+
+namespace H3D {
+
+  /// \ingroup X3DNodes
+  /// \class GeometryGroup
+  /// \brief The GeometryGroup node is treated by the scenegraph as one geometry
+	/// but it can contain several geometries.
+  ///
+  ///
+  /// <b>Examples:</b>
+  ///   - <a href="../../../H3DAPI/examples/All/GeometryGroup.x3d">GeometryGroup.x3d</a>
+  ///     ( <a href="examples/GeometryGroup.x3d.html">Source</a> )
+  ///
+  /// \par Internal routes:
+  /// \dotfile GeometryGroup.dot
+  class H3DAPI_API GeometryGroup : public X3DGeometryNode {
+  public:
+
+		class H3DAPI_API MFGeometry :
+      public DependentMFNode< X3DGeometryNode,
+        FieldRef< H3DDisplayListObject,
+                  H3DDisplayListObject::DisplayList,
+                  &H3DDisplayListObject::displayList >,
+                              true >  {
+      public:
+      /// The base class of MFChild.
+      typedef DependentMFNode< 
+        X3DGeometryNode,
+        FieldRef< H3DDisplayListObject,
+                  H3DDisplayListObject::DisplayList,
+                  &H3DDisplayListObject::displayList >,
+        true >  MFGeometryBase;
+
+      /// Destructor. Clears the MFNode in order to get the correct onRemove 
+      /// function to be called upon destruction. Must be done in all subclasses
+      /// that overrides onRemove ().
+      ~MFGeometry() {
+        clear();
+      }
+
+      /// Set up the routes we want to maintain.
+      virtual void onAdd( Node *n );
+
+      /// Remove the routes we want to maintain.
+      virtual void onRemove( Node *n );
+
+    private:
+      friend class X3DGeometryNode;
+    };
+    
+    /// SFBound is specialized to update from the SFBound fields 
+    /// routed to it. The resulting Bound object is the union of 
+    /// the Bound objects routed to it. If the bboxSize of the 
+    /// X3DGrouping node containing the SFBound field is ( -1, -1, -1 )
+    /// bound fields of all Nodes in the children field of the containing
+    /// Node that are instances of X3DBoundedObject are routed to it.
+    /// Otherwise the bound will be a BoxBound with center and
+    /// radius specified with the bboxCenter and bboxSize fields.
+    ///
+    class H3DAPI_API SFBound: 
+      public TypedField< X3DBoundedObject::SFBound,
+                         void,
+                         AnyNumber< X3DBoundedObject::SFBound > > {
+      /// The SFBound is updated to a bound that is the union of the 
+      /// the Bound objects routed to it.
+      virtual void update();
+    };
+
+		/// Specialized field class which grabs the first true value
+		/// for each index for any of the contained geometries. The geometry which
+		/// is responsible for the true value is then used for the contactPoint,
+		/// contactNormal and force fields. This is to make sure that the contact
+		/// values for each haptics device is consistent and belongs to one contact
+		/// point.
+		class H3DAPI_API IsTouched : public MFBool {
+		protected:
+			virtual void update();
+		};
+
+		/// Specialized field class which gets one value from each routed field
+		/// and stores that at the corresponding index in its own value. The index to use
+		/// is the index order of the routes in.
+		class H3DAPI_API MFVec3fPerIndex :
+			public TypedField< MFVec3f, MFVec3f, AnyNumber< MFVec3f > > {
+		public:
+			void unrouteAllFrom();
+		protected:
+			virtual void update();
+		};
+
+    /// Constructor.
+    GeometryGroup( Inst< SFNode > _metadata = 0,
+									 Inst< SFBound > _bound = 0,
+									 Inst< DisplayList > _displayList = 0,
+                   Inst< IsTouched   > _isTouched = 0,
+                   Inst< MFVec3fPerIndex > _force = 0,
+                   Inst< MFVec3fPerIndex > _contactPoint = 0,
+                   Inst< MFVec3fPerIndex > _contactNormal = 0,
+                   Inst< MFVec3fPerIndex > _contactTexCoord = 0,
+                   Inst< SFBoundTree > _boundTree = 0,
+									 Inst< MFGeometry > _geometry = 0 );
+
+    /// The number of triangles renderered in this geometry.
+    virtual int nrTriangles();
+
+    /// Renders the GeometryGroup with OpenGL.
+    virtual void render();
+
+    /// Traverse the scenegraph. Or rather, all its child geometries.
+    virtual void traverseSG( TraverseInfo &ti );
+
+    /// Detect intersection between a line segment and the sphere.
+    /// \param from The start of the line segment.
+    /// \param to The end of the line segment.
+    /// \param result Contains info about the closest intersection for every
+    /// object that intersects the line.
+    /// \returns true if intersected, false otherwise.
+    virtual bool lineIntersect( 
+      const Vec3f &from,
+      const Vec3f &to,    
+      LineIntersectResult &result );
+
+    /// Find closest point on the sphere to point p.
+    /// \param p The point to find the closest point to.
+    /// \param result A struct containing various results of closest
+    /// points such as which geometries the closest points where
+    /// detected on.
+    virtual void closestPoint( const Vec3f &p,
+                               NodeIntersectResult &result );
+
+    /// Detect collision between a moving sphere and this sphere.
+    /// \param radius The radius of the sphere
+    /// \param from The start position of the sphere
+    /// \param to The end position of the sphere.
+    /// \param result A struct containing various results of intersections
+    /// such as which geometries intersected the moving sphere.
+    /// \returns true if intersected, false otherwise.
+    virtual bool movingSphereIntersect( H3DFloat radius,
+                                        const Vec3f &from, 
+                                        const Vec3f &to,
+                                        NodeIntersectResult &result );
+
+
+		/// Returns true if this geometry supports the automatic generation
+    /// of tangents and binormals as FloatVertexAttribues(needed by
+    /// e.g. PhongShader.
+    virtual bool supportsTangentAttributes();
+
+		/// Function overridden from HAPIGLShape. Just call the 
+    /// displayList->callList of contained geometries.
+    virtual void glRender();
+
+    /// The geometry nodes.
+    ///
+    /// <b>Access type:</b> inputOutput \n
+    /// 
+    /// \dotfile GeometryGroup_geometry.dot
+    auto_ptr< MFGeometry > geometry;
+
+    /// The H3DNodeDatabase for this node.
+    static H3DNodeDatabase database;
+  };
+}
+
+#endif
---
+++ src/GeometryGroup.cpp
@@ -0,0 +1,256 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2013, SenseGraphics AB
+//
+//    This file is part of H3D API.
+//
+//    H3D API is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    H3D API is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with H3D API; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//    A commercial license is also available. Please contact us at 
+//    www.sensegraphics.com for more information.
+//
+//
+/// \file GeometryGroup.cpp
+/// \brief CPP file for GeometryGroup, X3D scene-graph node
+///
+//
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <H3D/GeometryGroup.h>
+
+using namespace H3D;
+
+// Add this node to the H3DNodeDatabase system.
+H3DNodeDatabase GeometryGroup::database( 
+                                 "GeometryGroup", 
+                                 &(newInstance<GeometryGroup>), 
+                                 typeid( GeometryGroup ),
+                                 &X3DGeometryNode::database );
+
+namespace GeometryGroupInternals {
+  FIELDDB_ELEMENT( GeometryGroup, geometry, INPUT_OUTPUT );
+}
+
+GeometryGroup::GeometryGroup( Inst< SFNode > _metadata,
+															Inst< SFBound > _bound,
+															Inst< DisplayList > _displayList,
+															Inst< IsTouched   > _isTouched,
+															Inst< MFVec3fPerIndex > _force,
+															Inst< MFVec3fPerIndex > _contactPoint,
+															Inst< MFVec3fPerIndex > _contactNormal,
+															Inst< MFVec3fPerIndex > _contactTexCoord,
+															Inst< SFBoundTree > _boundTree,
+															Inst< MFGeometry > _geometry ) :
+  X3DGeometryNode( _metadata, _bound, _displayList, _isTouched,
+									 _force, _contactPoint, _contactNormal,
+									 _contactTexCoord, _boundTree ),
+  geometry  ( _geometry ) {
+
+  type_name = "GeometryGroup";
+  database.initFields( this );
+
+  geometry->route( displayList );
+}
+
+int GeometryGroup::nrTriangles() {
+	int nr_triangles = 0;
+	const NodeVector &geometries = geometry->getValue();
+  for( unsigned int i = 0; i < geometries.size(); ++i ) {
+    if( geometries[i] ) {
+      nr_triangles += static_cast< X3DGeometryNode * >(geometries[i])->nrTriangles();
+    }
+  }
+	return nr_triangles;
+}
+
+void GeometryGroup::render() {
+  const NodeVector &geometries = geometry->getValue();
+  for( unsigned int i = 0; i < geometries.size(); ++i ) {
+    if( geometries[i] ) {
+      static_cast< X3DGeometryNode * >(geometries[i])->displayList->callList();
+    }
+  }
+}
+
+void GeometryGroup::traverseSG( TraverseInfo &ti ) {
+	const NodeVector &geometries = geometry->getValue();
+  for( unsigned int i = 0; i < geometries.size(); ++i ) {
+    if( geometries[i] ) {
+      geometries[i]->traverseSG(ti);
+    }
+  }
+}
+
+bool GeometryGroup::lineIntersect(
+                  const Vec3f &from, 
+                  const Vec3f &to,    
+                  LineIntersectResult &result ) {
+	bool intersect = false;
+  Bound * the_bound = bound->getValue();
+	if( !the_bound ||
+      the_bound->lineSegmentIntersect( from, to ) ) {
+    const NodeVector &geometries = geometry->getValue();
+    for( unsigned int i = 0; i < geometries.size(); ++i ) {
+      if( geometries[i] &&
+          geometries[i]->lineIntersect( from,
+                                        to,
+                                        result ) ) {
+          intersect = true;
+      }
+    }
+  }
+	return intersect;
+}
+
+void GeometryGroup::closestPoint( const Vec3f &p,
+                           NodeIntersectResult &result ) {
+  const NodeVector &geometries = geometry->getValue();
+  for( unsigned int i = 0; i < geometries.size(); ++i ) {
+    if( geometries[i] )
+      geometries[i]->closestPoint( p, result );
+  }
+}
+
+bool GeometryGroup::movingSphereIntersect( H3DFloat radius,
+                                    const Vec3f &from, 
+                                    const Vec3f &to,
+                                    NodeIntersectResult &result ) {
+  Bound * the_bound = bound->getValue();
+  if( !the_bound || the_bound->movingSphereIntersect( from, to, radius ) ) {
+    const NodeVector &geometries = geometry->getValue();
+    bool hit = false;
+    for( unsigned int i = 0; i < geometries.size(); ++i ) {
+      if( geometries[i] &&
+          geometries[i]->movingSphereIntersect( radius, from, to, result ))
+        hit = true;
+    }
+    return hit;
+  }
+  return false;
+}
+
+bool GeometryGroup::supportsTangentAttributes() {
+	const NodeVector &geometries = geometry->getValue();
+	for( unsigned int i = 0; i < geometries.size(); ++i )
+    if( geometries[i] && static_cast< X3DGeometryNode * >(geometries[i])->
+													supportsTangentAttributes())
+				return true;
+	return false;
+}
+
+void GeometryGroup::glRender() {
+	const NodeVector &geometries = geometry->getValue();
+	for( unsigned int i = 0; i < geometries.size(); ++i )
+    if( geometries[i] )
+			static_cast< X3DGeometryNode * >(geometries[i])->glRender();
+}
+
+void GeometryGroup::MFGeometry::onAdd( Node *n ) {
+  MFGeometryBase::onAdd( n );
+  GeometryGroup *o = static_cast< GeometryGroup* >( owner );
+  if ( n ) {
+		X3DGeometryNode *geom = static_cast< X3DGeometryNode * >(n);
+    geom->bound->route( o->bound );
+		geom->isTouched->route( o->isTouched, o->id );
+  }
+}
+
+void GeometryGroup::MFGeometry::onRemove( Node *n ) {
+  GeometryGroup *o = static_cast< GeometryGroup* >( owner );
+  if ( n ) {
+		X3DGeometryNode *geom = static_cast< X3DGeometryNode * >(n);
+    geom->bound->unroute( o->bound );
+		geom->isTouched->unroute( o->isTouched );
+		// Removing these routes in case they exist.
+		geom->force->unroute( o->force );
+		geom->contactPoint->unroute( o->contactPoint );
+		geom->contactNormal->unroute( o->contactNormal );
+		geom->contactTexCoord->unroute( o->contactTexCoord );
+  }
+  MFGeometryBase::onRemove( n );
+}
+
+void GeometryGroup::SFBound::update() {
+  value = Bound::SFBoundUnion( routes_in.begin(),
+                               routes_in.end() );
+}
+
+void GeometryGroup::IsTouched::update() {
+	vector< int > route_index;
+	value.clear();
+	GeometryGroup *o = static_cast< GeometryGroup* >( owner );
+	FieldVector force_routes_in = o->force->getRoutesIn();
+	const NodeVector &geometries = o->geometry->getValue();
+	bool update_field_routes = false;
+	for( unsigned int i = 0; i < routes_in.size(); ++i ) {
+		const vector< bool > &v = static_cast< MFBool *>(routes_in[i])->getValue();
+		if( value.size() < v.size() ) {
+			value.resize( v.size(), false );
+			route_index.resize( v.size(), i );
+		}
+		for( unsigned int j = 0; j < v.size(); j++ ) {
+			if( !value[j] && v[j] ) {
+				value[j] = true;
+				route_index[j] = i;
+				if( i >= force_routes_in.size() ||
+						static_cast< X3DGeometryNode * >(geometries[i])->force.get() !=
+						force_routes_in[i] )
+					update_field_routes = true;
+			}
+		}
+		// Even if every value in the vector is true we can't exit the loop early
+		// in case any of the routes that is later in the list have a isTouched field
+		// which is bigger than the current size.
+	}
+
+	if( update_field_routes ) {
+		static_cast< MFVec3fPerIndex *>(o->force.get())->unrouteAllFrom();
+		static_cast< MFVec3fPerIndex *>(o->contactPoint.get())->unrouteAllFrom();
+		static_cast< MFVec3fPerIndex *>(o->contactNormal.get())->unrouteAllFrom();
+		static_cast< MFVec3fPerIndex *>(o->contactTexCoord.get())->unrouteAllFrom();
+		for( unsigned int i = 0; i < route_index.size(); ++i ) {
+			X3DGeometryNode *geom =
+			 static_cast< X3DGeometryNode * >(geometries[route_index[i]]);
+			if( i == route_index.size() - 1 ) {
+				geom->force->route( o->force, o->id );
+				geom->contactPoint->route( o->contactPoint, o->id );
+				geom->contactNormal->route( o->contactNormal, o->id );
+				geom->contactTexCoord->route( o->contactTexCoord, o->id );
+			} else {
+				geom->force->routeNoEvent( o->force, o->id );
+				geom->contactPoint->routeNoEvent( o->contactPoint, o->id );
+				geom->contactNormal->routeNoEvent( o->contactNormal, o->id );
+				geom->contactTexCoord->routeNoEvent( o->contactTexCoord, o->id );
+			}
+			Console(3) << "index i: " << route_index[i] << endl;
+		}
+	}
+}
+
+void GeometryGroup::MFVec3fPerIndex::unrouteAllFrom() {
+	if( !routes_in.empty() ) {
+		int prev_routes_in_size;
+		do {
+			prev_routes_in_size = routes_in.size();
+			routes_in.front()->unroute( this );
+		} while( !routes_in.empty() || prev_routes_in_size == routes_in.size() );
+	}
+}
+
+void GeometryGroup::MFVec3fPerIndex::update() {
+	value.resize( routes_in.size() );
+	for( unsigned int i = 0; i < routes_in.size(); ++i )
+		value[i] = static_cast< MFVec3f * >(routes_in[i])->getValueByIndex(i);
+}
