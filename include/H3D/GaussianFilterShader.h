d4bcb96dc7e9483d538dac35e6b38464922a2ffc
daniel <daniel@610dfd11-2700-0410-9b68-9ae28eac5983>
2014-03-25 14:43:23 +0000

    Added GaussianFilterShader node
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@2601 610dfd11-2700-0410-9b68-9ae28eac5983

---
+++ include/H3D/GaussianFilterShader.h
@@ -0,0 +1,102 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2013, SenseGraphics AB
+//
+//    Any use, or distribution, of this file without permission from the
+//    copyright holders is strictly prohibited. Please contact SenseGraphics,
+//    www.sensegraphics.com, for more information.
+//
+//
+/// \file GaussianFilterShader.h
+/// \brief Header file for GaussianFilterShader.
+///
+//
+//////////////////////////////////////////////////////////////////////////////
+#ifndef __GAUSSIANFILTERSHADER_H__
+#define __GAUSSIANFILTERSHADER_H__
+
+#include <H3D/MFFloat.h>
+#include <H3D/ConvolutionFilterShader.h>
+
+namespace H3D {
+  /// \ingroup H3DNodes
+  /// \class ConvolutionFilterShader
+  /// \brief The GaussianFilterShader is a ConvolutionFilterShader node where 
+  /// the weights are automatically calculated from a gaussian function.
+  ///
+  /// The function used is:
+  /// 
+  /// If type field is "HORIZONTAL" or "VERTICAL"
+  /// f(x) = (1/(2.0*pi*sigma*sigma)*exp(-(x*x)/(2*sigma*sigma)) )
+  ///
+  /// If type field is "FULL"
+  /// f(x,y) = (1/(2.0*pi*sigma*sigma)*exp(-(x*x+y*y)/(2*sigma*sigma)) )
+  /// 
+  /// The sigma field is the sigma value in the functions above and decides how
+  /// much blur the result will be. A higher value of sigma will give a higher 
+  /// blur.
+  ///
+  /// The kernelSize field specifies the size convolution kernel.
+  /// This referes to how many pixels should be sampled in each direction.
+  ///
+  /// <b>Examples:</b>
+  ///   - <a href="../../../H3DAPI/examples/All/GaussionFilterShader.x3d">GaussionFilterShader.x3d</a>
+  ///     ( <a href="examples/GaussionFilterShader.x3d.html">Source</a> )
+  ///   - <a href="../../../H3DAPI/examples/All/GaussionFilterShader_postprocess.x3d">GaussionFilterShader_postprocess.x3d</a>
+  ///     ( <a href="examples/GaussionFilterShader_postprocess.x3d.html">Source</a> )
+  ///
+  /// \par Internal routes:
+  /// \dotfile GaussionFilterShader.dot
+  class H3DAPI_API GaussianFilterShader : 
+    public ConvolutionFilterShader {
+      /// Specialized field that reCalculate the kernel when it updates
+  public:
+      
+    struct MFWeights : 
+        public TypedField< MFFloat, void,  AnyNumber< Field > > {
+          virtual void update();
+        };
+
+    /// Constructor.
+    GaussianFilterShader( Inst< DisplayList  > _displayList = 0,
+                          Inst< SFNode       > _metadata    = 0,
+                          Inst< SFBool       > _isSelected  = 0,
+                          Inst< SFBool       > _isValid     = 0,
+                          Inst< SFBool       > _activate    = 0,
+                          Inst< SFString     > _language    = 0,
+                          Inst< MFShaderPart > _parts       = 0,
+                          Inst< SFBool       > _suppressUniformWarnings = 0,
+                          Inst< MFString     > _fragmentShaderString    = 0,
+                          Inst< MFString     > _vertexShaderString      = 0,
+                          Inst< SFTexture2DNode > _texture = 0,
+                          Inst< SFString     > _type        = 0,
+                          Inst< MFWeights      > _weights     = 0,
+                          Inst< SFInt32      > _kernelSize  = 0,
+                          Inst< SFFloat      > _sigma  = 0);
+
+    /// The sigma field is the sigma value in the gaussian function and decides how
+    /// much blur the result will be. A higher value of sigma will give a higher 
+    /// blur.
+    ///
+    /// <b>Access type:</b> inputOutput
+    /// <b>Default value:</b> 1
+    auto_ptr< SFFloat > sigma;
+
+
+    /// The H3DNodeDatabase for this node.
+    static H3DNodeDatabase database;
+  protected:
+
+    virtual bool canBuildShader();
+
+    /// regenerate the kernel. 
+    //virtual void generateKernel();
+
+  private:
+    std::vector<float> gauss2D(int x,int y, float sigma);
+    std::vector<float> gauss1D(int x,float sigma);
+
+
+  };
+}
+
+#endif
---
+++ src/GaussianFilterShader.cpp
@@ -0,0 +1,139 @@
+//////////////////////////////////////////////////////////////////////////////
+//    Copyright 2004-2013, SenseGraphics AB
+//
+//    Any use, or distribution, of this file without permission from the
+//    copyright holders is strictly prohibited. Please contact SenseGraphics,
+//    www.sensegraphics.com, for more information.
+//
+//
+/// \file GaussianFilterShader.cpp
+/// \brief CPP file for GaussianFilterShader.
+///
+//
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <H3D/GaussianFilterShader.h>
+#include <H3D/GeneratedTexture.h>
+
+using namespace H3D;
+
+// Add this node to the H3DNodeDatabase system.
+H3DNodeDatabase GaussianFilterShader::database( 
+                                               "GaussianFilterShader", 
+                                               &newInstance< GaussianFilterShader >,
+                                               typeid( GaussianFilterShader ),
+                                               &H3DGeneratedFragmentShaderNode::database );
+
+namespace GaussianFilterShaderInternals {
+  FIELDDB_ELEMENT( GaussianFilterShader, texture, INPUT_OUTPUT );
+  FIELDDB_ELEMENT( GaussianFilterShader, type, INPUT_OUTPUT );
+  FIELDDB_ELEMENT( GaussianFilterShader, weights, OUTPUT_ONLY );
+  FIELDDB_ELEMENT( GaussianFilterShader, kernelSize, INPUT_OUTPUT );
+  FIELDDB_ELEMENT( GaussianFilterShader, sigma, INPUT_OUTPUT);
+}
+
+GaussianFilterShader::GaussianFilterShader( Inst< DisplayList  > _displayList,
+                                            Inst< SFNode          > _metadata,
+                                            Inst< SFBool          > _isSelected ,
+                                            Inst< SFBool          > _isValid,
+                                            Inst< SFBool          > _activate,
+                                            Inst< SFString        > _language,
+                                            Inst< MFShaderPart    > _parts,
+                                            Inst< SFBool          > _suppressUniformWarnings,
+                                            Inst< MFString        > _fragmentShaderString,
+                                            Inst< MFString        > _vertexShaderString,
+                                            Inst< SFTexture2DNode > _texture,
+                                            Inst< SFString        > _type,
+                                            Inst< MFWeights       > _weights,
+                                            Inst< SFInt32         > _kernelSize,
+                                            Inst< SFFloat         > _sigma) :
+ConvolutionFilterShader( _displayList, _metadata, _isSelected, 
+                         _isValid, _activate, _language, _parts, 
+                         _suppressUniformWarnings, _fragmentShaderString, 
+                         _vertexShaderString, _texture, _type, _weights,_kernelSize ),
+  sigma(_sigma) {
+  
+  type_name = "GaussianFilterShader";
+  database.initFields( this );
+  sigma->setValue(1.0);
+  sigma->route( weights, id );
+  kernelSize->route( weights, id );
+  type->route( weights, id );
+}
+
+std::vector<float> GaussianFilterShader::gauss2D(int width, int height, float sigma) {
+  float sigmaSqr = sigma*sigma;
+  int nr_weights = width*height;
+  int midX = int(width/2);
+  int midY = int(height/2);
+  float sum = 0;
+  std::vector<float> result(nr_weights,0.0);
+  for(int  x=0; x<width;x++) {
+    for(int y=0;y<height;y++) {
+      // make use of the symmetry to simplify the calclation
+      // result[x*sizeX+y] is the xth row, yth column
+      float g = 0;
+      if(x>midX&&y>midY)
+        g = result[(width-x-1)*width+(height-y-1)];
+      else if(x>midX&&y<midY)
+        g = result[(width-x-1)*width+y];
+      else if(x<midX&&y>midY)
+        g = result[x*width+(height-y-1)];
+      else
+        g = (float) ( 1/(2.0*H3DUtil::Constants::pi*sigma*sigma)*H3DUtil::H3DExp(-(x*x+y*y)/(2*sigma*sigma)) );
+      result[x*width+y] = g;
+      sum += g;
+    }
+  }
+
+  for(int i = 0; i<nr_weights;i++) {
+    result[i] = result[i]/sum;
+  }
+  return result;
+}
+
+std::vector<float> GaussianFilterShader::gauss1D(int width, float sigma) {
+  float sum = 0;
+  int mid= (width-1)/2;
+  std::vector<float> result(width,0.0);
+  for(int i=0;i<width;i++ ) {
+    int current_index = i-mid;
+    float g = 0;
+    g= 1/(H3DUtil::H3DSqrt(2*H3DUtil::Constants::pi)*sigma) * H3DUtil::H3DExp(-current_index*current_index/(2.0f*sigma*sigma));
+    result.at(i) = g;
+    sum+=g;
+  }
+  for(int i = 0; i<width;i++) {
+    result[i] = result[i]/sum;
+  }
+
+  return result;
+}
+
+bool GaussianFilterShader::canBuildShader(){
+  // if kernel size is even or less than zero or sigma is less than zero, 
+  // no need to build shader.
+  return sigma->getValue()>0&&ConvolutionFilterShader::canBuildShader();
+}
+
+void GaussianFilterShader::MFWeights::update() { 
+  GaussianFilterShader *gfs =
+    static_cast< GaussianFilterShader * >( getOwner());
+ 
+  int demand_kernel_size = gfs->kernelSize->getValue();
+  float demand_sigma = gfs->sigma->getValue();
+  if( demand_sigma<0 || demand_kernel_size<0 || demand_kernel_size%2 == 0 ) {
+    // no need to update kernel
+    Console(2)<<"Invalid sigma or kernel size assigned, will not update kernel."<<endl;
+    return;
+  }
+
+  const string &type = gfs->type->getValue();
+
+  if ( type == "FULL") {
+    value = gfs->gauss2D(demand_kernel_size ,demand_kernel_size ,demand_sigma);
+  } else if( type == "VERTICAL"|| type == "HORIZONTAL") {
+    value = gfs->gauss1D(demand_kernel_size,demand_sigma);
+  }
+}
