b7fc4cf49f6393daab58be4f5b063fe94c6a34ab
hao <hao@610dfd11-2700-0410-9b68-9ae28eac5983>
2015-04-08 11:06:15 +0000

    update: re-apply revision 2899 with certain bug fix, now it is safe to use separate clear color for color buffer.
    git-svn-id: https://www.h3d.org:8090/H3DAPI/trunk/H3DAPI@2901 610dfd11-2700-0410-9b68-9ae28eac5983

--- include/H3D/FrameBufferTextureGenerator.h
+++ include/H3D/FrameBufferTextureGenerator.h
@@ -41,6 +41,8 @@
 #include <H3D/FieldTemplates.h>
 #include <H3D/SFColorRGBA.h>
 #include <H3D/Field.h>
+#include <H3D/MFColorRGBA.h>
+#include <H3DUtil/AutoRef.h>
 
 namespace H3D {
 
@@ -236,7 +238,8 @@ namespace H3D {
                                  Inst< SFInt32          > _scissorBoxX = 0,
                                  Inst< SFInt32          > _scissorBoxY = 0,
                                  Inst< SFInt32          > _scissorBoxWidth = 0,
-                                 Inst< SFInt32          > _scissorBoxHeight = 0);
+                                 Inst< SFInt32          > _scissorBoxHeight = 0,
+                                 Inst< MFColorRGBA      > _clearColors = 0);
         
     /// Destructor.
     virtual ~FrameBufferTextureGenerator();
@@ -544,6 +547,12 @@ namespace H3D {
     /// Specified clearColor used when useSpecifiedClearColor is true
     auto_ptr< SFColorRGBA > clearColor;
 
+    /// Specified clearColors
+    /// when there are more than one color output, use clearColors instead of
+    /// clearColor as the source of clear colors for the frame buffer
+    /// if clearColors is empty, then use clearColor to clear all color attachment
+    auto_ptr< MFColorRGBA > clearColors;
+
     auto_ptr< SFBool > useDSA;
 
     /// An option to tell FBTG to split the children scene and render each shape into 
@@ -833,7 +842,9 @@ namespace H3D {
 
       bool use_depth_stencil;
 
-      float* clear_color_value;
+      auto_ptr<float> clear_color_value;
+
+      //float* clear_color_value;
       
       
   };
